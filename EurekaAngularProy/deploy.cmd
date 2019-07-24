d6f582bf9fe7f176e4bc506517c57bfded7e86a9
PolarNemo <host29.3@gmail.com>
2019-07-23 22:16:03 -0500

    0.2

--- EurekaAngularProy/deploy.cmd
+++ EurekaAngularProy/deploy.cmd
@@ -105,6 +105,14 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
+:: 3. RUN npm packages
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! run-script build
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
 
