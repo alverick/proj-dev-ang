e6df1480f5e0ced0f33420a83e443972a8dcc27c
cacuna@CCitdata.com.pe <cacuna@itdata.com.pe>
2019-07-25 11:24:59 -0500

    inicio sp2

--- EurekaAngularProy/src/app/app-routing.module.ts
+++ EurekaAngularProy/src/app/app-routing.module.ts
@@ -14,10 +14,10 @@ import { AuthGuard } from 'src/app/shared/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent  },  
-  { path: 'login', component: LoginComponent,  canActivate: [AuthGuard]},
-  { path: '', redirectTo: '/login', pathMatch: 'full'  },  
+  { path: 'login', component: LoginComponent/*,  canActivate: [AuthGuard]*/},
+  /*{ path: '', redirectTo: '/login', pathMatch: 'full'  },  
   { path: '**', redirectTo: ''},
-
+*/
   { path: 'afiliacion', component: AfiliacionComponent},
   { path: 'configurarServicios', component: ConfigurarServiciosComponent},
   { path: 'crearContrasena', component: CrearContrasenaComponent},
