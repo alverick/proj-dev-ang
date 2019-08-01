9f982d1795633024753a4fefa9be78b81d588e0e
cacuna@CCitdata.com.pe <cacuna@itdata.com.pe>
2019-08-01 10:44:03 -0500

    fix 6 intento y navbar login

--- EurekaAngularProy/src/app/auth/login/login.component.ts
+++ EurekaAngularProy/src/app/auth/login/login.component.ts
@@ -204,7 +204,7 @@ export class LoginComponent implements OnInit {
           }else if(this.intentos >= 6){
             this.codigo2= false;
             console.log("Intentos : " + value.paramNum + "   Sin codigo");
-            Swal.fire({ type:"error", text: 'Lo sentimos tu contraseña es incorrecta, verifícala o vuelve a intentarlo. Tienes  '+this.intentosRestantes+' intentos restantes', showConfirmButton: false, showCloseButton: true});
+            Swal.fire({ type:"error", text: 'Contraseña Incorrecta” y texto: “Tu cuenta ha sido bloqueada por seguridad, inténtalo nuevamente en 60 minutos. Si tienes problemas para ingresar a tu cuenta, contáctanos a pilotos@intercorp.com.pe ', showConfirmButton: false, showCloseButton: true});
             this.intento6= true;
             this.isTrue = false;
                           
