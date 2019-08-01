0ded8df551c8d033d7a714a3bbc88650488ec673
cacuna@CCitdata.com.pe <cacuna@itdata.com.pe>
2019-08-01 14:49:36 -0500

    before merging

--- EurekaAngularProy/src/app/auth/login/login.component.ts
+++ EurekaAngularProy/src/app/auth/login/login.component.ts
@@ -34,13 +34,14 @@ export class LoginComponent implements OnInit {
   inputUsuaValid: boolean = false;
   inputPassValid: boolean = false;
 
-  intentos: number = 0;
+  intentos: number;
   intentosRestantes: number = 6;
   codRespuesta: number;
   err: boolean;
   numero2: number;
   checked: boolean = false;
   intento6: boolean =false;
+  ruc: number=0;
 
   isTrue: boolean = false;
   codigo2: boolean = false;
@@ -132,8 +133,11 @@ export class LoginComponent implements OnInit {
     this.inputPassValid = false; 
   }
   /* /////// L O G I N ////////////  */
- 
+
+    
+
   public submitLogin() : any { 
+    console.log("INTENTOS : "+this.intentos)
 
     if(this.inputUsua.nativeElement.value === '' && this.inputPass.nativeElement.value === '') {
       this.inputUsuaValid = true;
@@ -157,7 +161,14 @@ export class LoginComponent implements OnInit {
           this.codRespuesta= value.codRespuesta;
           if(value.paramStr==="Un session ya se encuentra activa"){
             Swal.fire({ type: 'warning', text: 'Existe una Sesión Activa'})
-          }else if(value.estado===true){
+          }else if(this.intentos >= 6){
+            this.codigo2= false;
+            console.log("Intentos : " + value.paramNum + "   Sin codigo");
+            Swal.fire({ type:"error", text: 'Contraseña Incorrecta” y texto: “Tu cuenta ha sido bloqueada por seguridad, inténtalo nuevamente en 60 minutos. Si tienes problemas para ingresar a tu cuenta, contáctanos a pilotos@intercorp.com.pe ', showConfirmButton: false, showCloseButton: true});
+            this.intento6= true;
+            this.isTrue = false;
+                          
+          }else if(value.estado===true && value.paramNum<=6){
                   if(this.rememberMe==true){
                       const expire = new Date();
                       expire.setDate(expire.getDate() + 25);
@@ -201,14 +212,7 @@ export class LoginComponent implements OnInit {
             this.isCaptchaValidate = false;
             this.isTrue = true; 
             this.codigo2= false;             
-          }else if(this.intentos >= 6){
-            this.codigo2= false;
-            console.log("Intentos : " + value.paramNum + "   Sin codigo");
-            Swal.fire({ type:"error", text: 'Contraseña Incorrecta” y texto: “Tu cuenta ha sido bloqueada por seguridad, inténtalo nuevamente en 60 minutos. Si tienes problemas para ingresar a tu cuenta, contáctanos a pilotos@intercorp.com.pe ', showConfirmButton: false, showCloseButton: true});
-            this.intento6= true;
-            this.isTrue = false;
-                          
-          }            
+          }         
         },
       error =>{ 
         this.spinner.hide();
