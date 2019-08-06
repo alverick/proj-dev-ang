c973081d6794f46ec6b58ce60fab992ee51145b4
mnievafra <mnieva@itdata.com.pe>
2019-08-06 16:08:01 -0500

    changes login message

--- EurekaAngularProy/src/app/auth/login/login.component.ts
+++ EurekaAngularProy/src/app/auth/login/login.component.ts
@@ -191,7 +191,7 @@ export class LoginComponent implements OnInit {
             this.codigo2= false;
           
             console.log("Intentos : " + value.paramNum + "  Codigo de Respuesta 3");
-            Swal.fire({ type:"error", text: 'Lo sentimos tu contraseña es incorrecta, verifícala o vuelve a intentarlo. Tienes  '+this.intentosRestantes+' intentos restantes', showConfirmButton: false, showCloseButton: true}); 
+            Swal.fire({ type:"error", title:"Contraseña incorrecta" ,text: 'Lo sentimos tu contraseña es incorrecta, verifícala o vuelve a intentarlo. Tienes  '+this.intentosRestantes+' intentos restantes', showConfirmButton: false, showCloseButton: true}); 
           }else if(this.intentos == 4 && this.codRespuesta == 2){
             console.log("Intentos : " + value.paramNum + "   Codigo de Respuesta 2");
             this.loginService.errores= value.codRespuesta;            
@@ -203,7 +203,7 @@ export class LoginComponent implements OnInit {
           }else if(this.intentos == 4 && this.codRespuesta == 3){
             this.codigo2= false;
             console.log("Intentos : " + value.paramNum + "   Codigo de Respuesta 3");
-            Swal.fire({ type:"error", text: 'Lo sentimos tu contraseña es incorrecta, verifícala o vuelve a intentarlo. Tienes  '+this.intentosRestantes+' intentos restantes', showConfirmButton: false, showCloseButton: true});this.isCaptchaValidate = false;
+            Swal.fire({ type:"error",title:"Contraseña incorrecta" , text: 'Lo sentimos tu contraseña es incorrecta, verifícala o vuelve a intentarlo. Tienes  '+this.intentosRestantes+' intentos restantes', showConfirmButton: false, showCloseButton: true});this.isCaptchaValidate = false;
             this.recaptchaRef !== undefined ? this.recaptchaRef.reset() : null;
             this.isTrue = true;              
             
@@ -215,7 +215,7 @@ export class LoginComponent implements OnInit {
           }else if(this.intentos == 5 && this.codRespuesta == 3){
             this.codigo2= false;
             console.log("Intentos : " + value.paramNum + "   Codigo de Respuesta 3");
-            Swal.fire({ type:"error", text: 'Lo sentimos tu contraseña es incorrecta, verifícala o vuelve a intentarlo. Tienes  '+this.intentosRestantes+' intentos restantes', showConfirmButton: false, showCloseButton: true});        
+            Swal.fire({ type:"error",title:"Contraseña incorrecta" , text: 'Lo sentimos tu contraseña es incorrecta, verifícala o vuelve a intentarlo. Tienes  '+this.intentosRestantes+' intentos restantes', showConfirmButton: false, showCloseButton: true});        
             this.recaptchaRef !== undefined ? this.recaptchaRef.reset() : null;
             this.isCaptchaValidate = false;
             this.isTrue = true; 
@@ -224,7 +224,7 @@ export class LoginComponent implements OnInit {
           else if(this.intentos >= 6){
             this.codigo2= false;
             console.log("Intentos : " + value.paramNum + "   Sin codigo");
-            Swal.fire({ type:"error", text: 'Contraseña Incorrecta” y texto: “Tu cuenta ha sido bloqueada por seguridad, inténtalo nuevamente en 60 minutos. Si tienes problemas para ingresar a tu cuenta, contáctanos a pilotos@intercorp.com.pe ', showConfirmButton: false, showCloseButton: true});
+            Swal.fire({ type:"error"  text: 'Contraseña Incorrecta” y texto: “Tu cuenta ha sido bloqueada por seguridad, inténtalo nuevamente en 60 minutos. Si tienes problemas para ingresar a tu cuenta, contáctanos a pilotos@intercorp.com.pe ', showConfirmButton: false, showCloseButton: true});
             this.intento6= true;
             this.isTrue = false;             
           }         
