ad0f0aa3e817de265fc26e824406dff5bf88524a
PolarNemo <host29.3@gmail.com>
2019-10-03 15:12:40 -0500

    Updating for UnitTesting

--- EurekaAngularProy/src/environments/environment.local.ts
+++ EurekaAngularProy/src/environments/environment.local.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    END_POINT: 'https://localhost:5001/api/',
+    END_POINT: 'https://localhost:5001/api',
     OCP_KEY: "c9664a560c184e8cb857e5d2a7efabc4"
   };
 
