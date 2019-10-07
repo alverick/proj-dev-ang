273c9eb1d8cd03a47b8944dcb40467b0a9886382
mnievafra <mnieva@itdata.com.pe>
2019-10-07 11:07:39 -0500

    changes configurar empresa

--- EurekaAngularProy/src/app/pages/configurar-empresa/configurar-empresa.component.ts
+++ EurekaAngularProy/src/app/pages/configurar-empresa/configurar-empresa.component.ts
@@ -60,7 +60,7 @@ export class ConfigurarEmpresaComponent implements OnInit {
       entry: new FormControl({ value: '', disabled: true }),
       email: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z0-9]{1,}([-._]{1}[A-Za-z0-9]{1,})?@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$'), Validators.minLength(10), Validators.maxLength(100)]),
       movilNumber: new FormControl('', [Validators.required, Validators.pattern('^([9][0-9]{8})?([1-8][0-9]{5,6})?$'), Validators.minLength(6), Validators.maxLength(9)]),
-      password: new FormControl('',   [Validators.minLength(6), Validators.maxLength(20) ]),
+      password: new FormControl('',   [Validators.minLength(6), Validators.maxLength(20),UnaLetra ]),
       newPassword: new FormControl('',[Validators.minLength(6), Validators.maxLength(20), UnaLetra]),
       confirmNewPassword: new FormControl('',[Validators.minLength(6), Validators.maxLength(20), UnaLetra]),
     }, {
