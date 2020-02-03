da18d2dc6e45eaf34fa1fd4ee5e3195fa2e67561
PolarNemo <host29.3@gmail.com>
2020-02-03 16:59:59 -0500

    Column name in dashboard Changed

--- EurekaAngularProy/src/environments/environment.ts
+++ EurekaAngularProy/src/environments/environment.ts
@@ -8,15 +8,15 @@ export const environment = {
   OCP_KEY: "3b8700ab20814ee58e07ffc89e16c86d"*/
 
  ////////// D E V E L O P ///////////////////////*
- /* */
+ /* 
   END_POINT: 'http://40.84.21.50/api',
   OCP_KEY: "3b8700ab20814ee58e07ffc89e16c86d"
-/* */
+ */
 
 /*END_POINT: 'https://apis.dev.interbank.pe/eureca/api',
   OCP_KEY: "3b8700ab20814ee58e07ffc89e16c86d"*/
 
- /*END_POINT: 'https://localhost:5001/api',
+ /**/END_POINT: 'https://localhost:5001/api',
   OCP_KEY: "3b8700ab20814ee58e07ffc89e16c86d"
-*/
+
 }
