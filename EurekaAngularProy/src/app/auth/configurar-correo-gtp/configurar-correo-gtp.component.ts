e4cf6ba16f32ee343ec7c6318a43e20349603f3e
PolarNemo <host29.3@gmail.com>
2020-05-08 15:07:10 -0500

    ini-email

--- EurekaAngularProy/src/app/auth/configurar-correo-gtp/configurar-correo-gtp.component.ts
+++ EurekaAngularProy/src/app/auth/configurar-correo-gtp/configurar-correo-gtp.component.ts
@@ -112,7 +112,15 @@ export class ConfigurarCorreoGtpComponent implements OnInit {
     this.summited = true;
     if (this.frmCorreoGtp.valid) {
       if (this.indiceActual >= 0) {
-        this.correos[this.indiceActual].correo = this.frmCorreoGtp.value.correo;
+        if (this.correos.find((s, i) => s.correo.toUpperCase() === this.frmCorreoGtp.value.correo.toUpperCase() && i !== this.indiceActual)) {
+          Swal.fire({
+            text: 'Ya existe un E-mail con este nombre',
+            onOpen: drawPopup
+          });
+          return;
+        } else {
+          this.correos[this.indiceActual].correo = this.frmCorreoGtp.value.correo;
+        }
       } else {
         if (this.correos.find((s, i) => s.correo.toUpperCase() === this.frmCorreoGtp.value.correo.toUpperCase() && i !== this.indiceActual)) {
           Swal.fire({
