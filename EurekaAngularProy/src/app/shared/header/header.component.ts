b0b41bee7f16029612276cdc45c1424c41f2cfed
PolarNemo <host29.3@gmail.com>
2020-05-15 09:11:04 -0500

    ini-heder

--- EurekaAngularProy/src/app/shared/header/header.component.ts
+++ EurekaAngularProy/src/app/shared/header/header.component.ts
@@ -146,16 +146,16 @@ export class HeaderComponent implements OnInit {
     }
   }
   goBack() {
+
     if (this.router.url.includes('/cargaHistorico')) {
       this.router.navigate(['/gtp']);
-    }
-    else {
+    } else if (this.router.url.includes('/crearContrasena')) {
+      this.router.navigate(['/afiliacion']);
+    } else if (this.router.url.includes('/afiliacion')) {
+      this.router.navigate(['/login']);
+    } else {
       if (confirm('Es posible que los cambios no se guarden.')) {
         this.router.navigate(['/home']);
-      } else if (this.router.url.includes('/crearContrasena')) {
-        this.router.navigate(['/afiliacion']);
-      } else if (this.router.url.includes('/afiliacion')) {
-        this.router.navigate(['/login']);
       }
     }
   }
