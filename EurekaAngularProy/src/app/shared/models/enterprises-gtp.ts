c0766d26dd45f559e173bed54e5deddd213c228e
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-06-02 18:07:53 -0500

    CSBIE-38 fix(shared): eslint fixes

--- EurekaAngularProy/src/app/shared/models/enterprises-gtp.ts
+++ EurekaAngularProy/src/app/shared/models/enterprises-gtp.ts
@@ -1,17 +1,16 @@
 import { RequestTypegtp } from './request-typegtp';
 export interface EnterprisesGtp {
-      clientId: number ;
-      requestDate: Date; 
-      ruc: string;
-      cu: String;  
-      nameEnterprise:String;
-      status:  string; 
-      businessHeading:String;
-      requesType:string;
-      requestTypeList: RequestTypegtp[];
-     
+  clientId: number;
+  requestDate: Date;
+  ruc: string;
+  cu: string;
+  nameEnterprise: string;
+  status: string;
+  businessHeading: string;
+  requesType: string;
+  requestTypeList: RequestTypegtp[];
 }
-export interface EnterprisesPagedList { 
-      listCompanyGTP: EnterprisesGtp[];
-      totalCompanies: number;
-  }
+export interface EnterprisesPagedList {
+  listCompanyGTP: EnterprisesGtp[];
+  totalCompanies: number;
+}
--- EurekaAngularProy/src/app/shared/services/auth-interceptor.service.ts
+++ EurekaAngularProy/src/app/shared/services/auth-interceptor.service.ts
@@ -1,25 +1,39 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
-import Swal from 'sweetalert2';
 import { MatSnackBar } from '@angular/material';
-import { LoginService } from './login.service';
-import { StorageService } from './storage.service';
-import { drawPopup } from './popups';
+import { Router } from '@angular/router';
+import { throwError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+import { environment } from '../../../environments/environment';
+import { authFullRoutingNames } from '../../features/auth/auth-routing.names';
 import { GoogleAnalytics } from './googleAnalytics.service';
+import { LoginService } from './login.service';
+import { drawPopup } from './popups';
+import { StorageService } from './storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthInterceptorService implements HttpInterceptor {
+  constructor(
+    private router: Router,
+    public snackBar: MatSnackBar,
+    private login: LoginService,
+    private storage: StorageService,
+    private gaService: GoogleAnalytics
+  ) {}
 
-  constructor(private router: Router, public snackBar: MatSnackBar, private login: LoginService,
-    private storage: StorageService, private gaService: GoogleAnalytics) { }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     if (!req.url.includes('notification')) {
       this.login.refresh();
     }
@@ -29,24 +43,24 @@ export class AuthInterceptorService implements HttpInterceptor {
 
     let headers = {
       'Cache-Control': 'no-cache',
-      'Pragma': 'no-cache',
-      'Expires': 'Sat, 01 Jan 2000 00:00:00 GMT',
-      "Ocp-Apim-Subscription-Key": environment.OCP_KEY,
-      "Ocp-Apim-Trace": `true`
+      Pragma: 'no-cache',
+      Expires: 'Sat, 01 Jan 2000 00:00:00 GMT',
+      'Ocp-Apim-Subscription-Key': environment.OCP_KEY,
+      'Ocp-Apim-Trace': `true`,
     };
 
-    if(token){
-      headers["Authorization"] = `bearer ${token}`
+    if (token) {
+      headers['Authorization'] = `bearer ${token}`;
     }
-      request = req.clone({
-      setHeaders: headers
+    request = req.clone({
+      setHeaders: headers,
     });
 
-    return next.handle(request)
-      .pipe(catchError((err: HttpErrorResponse)=>{
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
         this.gaService.sendException(err);
         if (!request.url.includes('notification')) {
-          if(err.status === 401){
+          if (err.status === 401) {
             this.storage.removeCurrentSession();
             this.snackBar.dismiss();
             Swal.fire({
@@ -54,12 +68,12 @@ export class AuthInterceptorService implements HttpInterceptor {
               showCloseButton: true,
               showCancelButton: false,
               showConfirmButton: true,
-              confirmButtonText:  'CERRAR',
+              confirmButtonText: 'CERRAR',
               allowOutsideClick: false,
               onOpen: drawPopup,
-              onClose: () =>{
-                location.href = '/login';
-              }
+              onClose: () => {
+                location.href = authFullRoutingNames.LOGIN;
+              },
             });
           } else if (err.status !== 400) {
             this.storage.removeCurrentSession();
@@ -69,19 +83,17 @@ export class AuthInterceptorService implements HttpInterceptor {
               showCloseButton: true,
               showCancelButton: false,
               showConfirmButton: true,
-              confirmButtonText:  'CERRAR',
+              confirmButtonText: 'CERRAR',
               allowOutsideClick: false,
               onOpen: drawPopup,
-              onClose: () =>{
-                //this.router.navigateByUrl('/login')
-                location.href = '/login';
-              }
-            })
+              onClose: () => {
+                location.href = authFullRoutingNames.LOGIN;
+              },
+            });
           }
         }
         return throwError(err);
       })
     );
-
   }
 }
