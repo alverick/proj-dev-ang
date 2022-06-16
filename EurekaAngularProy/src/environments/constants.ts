b53bb3785a451f2d39c7ab6c5b8b8761bb04af93
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-06-16 18:54:33 -0500

    CSBIE-38 fix(environment): update endpoints for dev and uat

--- EurekaAngularProy/src/environments/constants.ts
+++ EurekaAngularProy/src/environments/constants.ts
@@ -1,6 +1,6 @@
 export const serverUrl = {
   local: 'https://localhost:5001/api',
-  dev: 'http://20.88.106.40/api',
-  uat: 'http://40.65.245.136/api',
+  dev: 'https://apis.dev.interbank.pe/eureca/api',
+  uat: 'https://apis.uat.interbank.pe/eureca/api',
   prod: 'https://apis.interbank.pe/eureca/api',
 };
