1eafe7e899da406c0e31b04adf91d5eb731cad21
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-06-16 14:23:18 -0500

    CSBIE-50 fix(directives): update name directive

--- EurekaAngularProy/src/app/features/internal/directives/date.directive.ts
+++ EurekaAngularProy/src/app/features/internal/directives/date.directive.ts
@@ -1,18 +1,16 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: 'input[appDate]'
+  selector: 'input[csDate]',
 })
 export class DateDirective {
+  constructor(private _el: ElementRef) {}
 
-  constructor(private _el: ElementRef) { }
-  
   @HostListener('input', ['$event']) onInputChange(event) {
-    const initalValue = this._el.nativeElement.value;
-    this._el.nativeElement.value = initalValue.replace(/[^0-9-/]*/g, '');
-    if ( initalValue !== this._el.nativeElement.value) {
+    const initialValue = this._el.nativeElement.value;
+    this._el.nativeElement.value = initialValue.replace(/[^0-9-/]*/g, '');
+    if (initialValue !== this._el.nativeElement.value) {
       event.stopPropagation();
     }
   }
-
 }
