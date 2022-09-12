4aaa51d4164bcae064a35e79ac5114f2c69f02e1
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-09-12 11:23:48 -0500

    CSBIE-164 fix(shared): lint fixes

--- EurekaAngularProy/src/app/shared/models/data-enterprise-gtp.ts
+++ EurekaAngularProy/src/app/shared/models/data-enterprise-gtp.ts
@@ -1,23 +1,23 @@
-import { DataServiceGTP } from "./data-service-gtp";
+import { DataServiceGTP } from './data-service-gtp';
 
 export interface DataEnterpriseGTP {
-    cu?: string;
-    ruc: string|number;
-    name: string;
-    entry: string;
-    entryName: string;
-    email: string;
-    movilNumber: number;
-    movilOperator: string;
-    newName: string;
-    newNameGTPStatus?: any;
-    status: string;
-    uniqueCodeIBK: String;
-    requestDate?: Date;
-    inReview?: boolean;
-    arrayServices?: DataServiceGTP[];
-    NombreApproved?: boolean;
-    enabled?: boolean;
-    isNewEnterprise?: boolean;
-    useAgencyChannel?: boolean;
+  cu?: string;
+  ruc: string | number;
+  name: string;
+  entry: string;
+  entryName: string;
+  email: string;
+  movilNumber: number;
+  movilOperator: string;
+  newName: string;
+  newNameGTPStatus?: any;
+  status: string;
+  uniqueCodeIBK: string;
+  requestDate?: Date;
+  inReview?: boolean;
+  arrayServices?: DataServiceGTP[];
+  NombreApproved?: boolean;
+  enabled?: boolean;
+  isNewEnterprise?: boolean;
+  useAgencyChannel?: boolean;
 }
