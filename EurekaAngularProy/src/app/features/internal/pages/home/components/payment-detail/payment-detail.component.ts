331d7415e8c2d38c905ad0c5fbc7710ceb35ad83
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-09-21 15:21:47 -0500

    CSBIE-185 fix(internal): update payment detail component

--- EurekaAngularProy/src/app/features/internal/pages/home/components/payment-detail/payment-detail.component.ts
+++ EurekaAngularProy/src/app/features/internal/pages/home/components/payment-detail/payment-detail.component.ts
@@ -1,68 +1,34 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import {
-  DateAdapter,
-  MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE,
-} from '@angular/material';
-import { MomentDateAdapter } from '@angular/material-moment-adapter';
-import * as _moment from 'moment'; // dejalo si sale error
-import { default as _rollupMoment } from 'moment';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { GoogleAnalytics } from 'src/app/shared/services/googleAnalytics.service';
-import { PagoService } from 'src/app/shared/services/pago.service';
 import { TransactionService } from 'src/app/shared/services/transaction.service';
 import { drawPopup } from 'src/app/shared/utils/helpers/popups';
 import Swal from 'sweetalert2';
-import { PopoverRef } from '../popover/popover-ref';
 
-const moment = _rollupMoment || _moment;
-
-const MY_FORMATS = {
-  parse: {
-    dateInput: 'DD/MM/YYYY',
-  },
-  display: {
-    dateInput: 'DD/MM/YYYY',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
-  },
-};
-
-declare var $: any;
 @Component({
-  selector: 'cs-pagos',
-  templateUrl: './pagos.component.html',
-  styleUrls: ['./pagos.component.scss'],
-  providers: [
-    {
-      provide: DateAdapter,
-      useClass: MomentDateAdapter,
-      deps: [MAT_DATE_LOCALE],
-    },
-    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
-  ],
+  selector: 'cs-payment-detail',
+  templateUrl: './payment-detail.component.html',
+  styleUrls: ['./payment-detail.component.scss'],
 })
-export class PagosComponent implements OnInit {
-  showed = false;
-
+export class PaymentDetailComponent implements OnInit {
   items: any[] = [];
-  cargando = false;
-
+  loading = false;
+  isEditingRow = false;
+  customer: any = {};
   debtId: number;
   status: string;
   currency: string;
-  @Output() statusChange = new EventEmitter<string>();
 
   constructor(
     private transaction: TransactionService,
-    private pagoService: PagoService,
-    private popoverRef: PopoverRef,
-    private gaService: GoogleAnalytics
+    private gaService: GoogleAnalytics,
+    public dialogRef: MatDialogRef<PaymentDetailComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    this.debtId = popoverRef.data.debtId;
-    this.status = popoverRef.data.status;
-    this.currency = popoverRef.data.currency;
+    this.debtId = data.debtId;
+    this.customer = data.customer;
+    this.status = data.status;
+    this.currency = data.currency;
   }
 
   ngOnInit(): void {
@@ -71,46 +37,32 @@ export class PagosComponent implements OnInit {
 
   private loadData() {
     this.items = [];
-    this.cargando = true;
+    this.loading = true;
     this.transaction.getPayments(this.debtId).subscribe((p) => {
-      this.cargando = false;
+      this.loading = false;
       this.items = p;
     });
   }
 
-  public show() {
-    if (!this.showed) {
-      this.pagoService.closeAll.emit();
-      this.showed = true;
-      this.loadData();
-    } else {
-      this.showed = false;
-    }
-  }
-
-  mensaje(tipo: any, titulo: string, text: string) {
-    if (sessionStorage.getItem('tk') !== null) {
-      this.mesageeError(tipo, titulo, text);
-    }
-  }
-
-  mesageeError(tipo: any, titulo: string, text: string) {
-    Swal.fire({
-      title: titulo,
-      html: text,
-      showCloseButton: true,
-      showCancelButton: false,
-      showConfirmButton: true,
-      confirmButtonText: 'CERRAR',
-      onOpen: drawPopup,
-    });
-  }
-
   editItm(itm) {
+    this.isEditingRow = true;
     itm.editing = true;
     itm.newAmount = itm.amount;
     itm.newDate = itm.date;
     itm.newChannel = itm.channel;
+    itm.errores = {};
+  }
+
+  saveItem() {
+    this.saveItm(this.getItemEditing());
+  }
+
+  private getItemEditing() {
+    return this.items.find(({ editing }) => editing);
+  }
+
+  cancelItem() {
+    this.cancelItm(this.getItemEditing());
   }
 
   saveItm(itm) {
@@ -120,17 +72,18 @@ export class PagosComponent implements OnInit {
     if (itm.newAmount.toString().length < 1) {
       itm.errores.amount = 'Ingrese un monto correcto';
     }
-    if (parseInt(itm.newAmount.toString()) < 0) {
+    if (parseInt(itm.newAmount.toString(), 10) < 0) {
       itm.errores.amount = 'Ingrese un monto correcto';
     }
 
-    if (!itm.newAmount.toString().match(/^[0-9]{1,9}([.][0-9]{0,2})?$/)) {
+    if (!itm.newAmount.toString().match(/^\d{1,9}([.]\d{0,2})?$/)) {
       itm.errores.amount = 'Ingrese un monto válido';
     }
 
-    var lenghted = new Date(itm.newDate).toDateString().length;
-    var newdate = parseInt(
-      new Date(itm.newDate).toDateString().substr(lenghted - 4, lenghted)
+    const lenghted = new Date(itm.newDate).toDateString().length;
+    const newdate = parseInt(
+      new Date(itm.newDate).toDateString().substr(lenghted - 4, lenghted),
+      10
     );
 
     if (newdate < 2000 || newdate > 2050) {
@@ -141,8 +94,10 @@ export class PagosComponent implements OnInit {
       itm.errores.date = 'Ingrese una fecha válida';
     }
 
-    for (var s in itm.errores) {
-      if (itm.errores[s]) return;
+    for (const s in itm.errores) {
+      if (itm.errores[s]) {
+        return;
+      }
     }
 
     Swal.fire({
@@ -160,7 +115,7 @@ export class PagosComponent implements OnInit {
           channel: itm.newChannel,
           amount: parseFloat(itm.newAmount.toString()),
         };
-        let response = itm.id
+        const response = itm.id
           ? this.transaction.editPayment(this.debtId, itm.id, payment)
           : this.transaction.addPayment(this.debtId, payment);
         response.subscribe((r) => {
@@ -177,8 +132,6 @@ export class PagosComponent implements OnInit {
               });
             }
             this.status = r.status;
-            //this.statusChange.emit(r.status);
-            this.popoverRef.changeStatus(r.status);
             this.loadData();
             Swal.fire({
               titleText: 'Editado!',
@@ -187,6 +140,7 @@ export class PagosComponent implements OnInit {
               showCancelButton: false,
               onOpen: drawPopup,
               onAfterClose: () => {
+                this.isEditingRow = false;
                 itm.editing = false;
                 itm.amount = itm.newAmount;
                 itm.date = itm.newDate;
@@ -208,6 +162,7 @@ export class PagosComponent implements OnInit {
   }
 
   cancelItm(itm) {
+    this.isEditingRow = false;
     if (itm.id === undefined) {
       this.items.pop();
     } else {
@@ -215,8 +170,8 @@ export class PagosComponent implements OnInit {
     }
   }
 
-  addItm() {
-    //if (this.items[this.items.length-1] && this.items[this.items.length-1].id) {
+  addItem() {
+    this.isEditingRow = true;
     this.items.push({
       currency: this.currency,
       newAmount: '',
@@ -226,7 +181,6 @@ export class PagosComponent implements OnInit {
       editing: true,
       errores: {},
     });
-    //}
   }
 
   delItm(itm) {
@@ -246,8 +200,6 @@ export class PagosComponent implements OnInit {
               event_label: 'eliminar_pagos',
             });
             this.status = r.status;
-            //this.statusChange.emit(r.status);
-            this.popoverRef.changeStatus(r.status);
             this.loadData();
           } else {
             Swal.fire({
@@ -263,8 +215,14 @@ export class PagosComponent implements OnInit {
     });
   }
 
-  AmountBlur(itm) {
-    let initalValue = parseFloat(itm.newAmount);
-    if (!isNaN(initalValue)) itm.newAmount = initalValue.toFixed(2);
+  amountBlur(itm) {
+    const initalValue = parseFloat(itm.newAmount);
+    if (!isNaN(initalValue)) {
+      itm.newAmount = initalValue.toFixed(2);
+    }
+  }
+
+  close() {
+    this.dialogRef.close({ status: this.status });
   }
 }
