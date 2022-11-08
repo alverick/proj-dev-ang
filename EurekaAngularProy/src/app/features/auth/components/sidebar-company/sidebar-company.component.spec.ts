dead9cc8d00967f4b5978a51c5972e0f60027270
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-11-08 09:40:45 -0500

    CSBIE-271 feat(auth): added sidebar company component

---
+++ EurekaAngularProy/src/app/features/auth/components/sidebar-company/sidebar-company.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SidebarCompanyComponent } from './sidebar-company.component';
+
+describe('SidebarInfoComponent', () => {
+  let component: SidebarCompanyComponent;
+  let fixture: ComponentFixture<SidebarCompanyComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [SidebarCompanyComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SidebarCompanyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
