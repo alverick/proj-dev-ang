fa63a4c56f61d0057c1d703bdeb4808c1e76fc4f
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-11-08 11:20:52 -0500

    CSBIE-271 feat(auth): add service list page

---
+++ EurekaAngularProy/src/app/features/auth/pages/service-list/service-list.page.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IErrorMessages } from '../../../../shared/models/forms';
+import { swalAlert } from '../../../../shared/utils/helpers/popups';
+import { authFullRoutingNames } from '../../auth-routing.names';
+import { AffiliationService } from '../../services/affiliation.service';
+
+@Component({
+  selector: 'cs-service-list',
+  templateUrl: './service-list.page.html',
+  styleUrls: ['./service-list.page.scss'],
+})
+export class ServiceListPage implements OnInit {
+  showSidebar = false;
+  position: number;
+  errorMessages: IErrorMessages;
+
+  constructor(private router: Router, public affiliation: AffiliationService) {}
+
+  ngOnInit() {
+    this.errorMessages = {
+      ...this.affiliation.errorMessagesServiceConfig,
+      ...this.affiliation.errorMessagesService,
+    };
+  }
+
+  actionDelete(position: number) {
+    swalAlert
+      .fire({
+        text: 'Se eliminará el servicio de los canales de interbank',
+        title: 'Eliminación total el servicio',
+        showCancelButton: true,
+        showConfirmButton: true,
+        confirmButtonText: 'CONFIRMAR',
+        cancelButtonText: 'CANCELAR',
+        allowOutsideClick: false,
+      })
+      .then(({ value }) => {
+        if (value) {
+          this.affiliation.deleteService(position);
+        }
+      });
+  }
+
+  actionEdit(position: number) {
+    this.affiliation.setEditForm(position);
+    this.position = position;
+    this.showSidebar = true;
+  }
+
+  finalize() {
+    this.affiliation.saveAllServices().subscribe(() => {
+      this.router.navigate([authFullRoutingNames.REGISTRATION_FINISHED]);
+    });
+  }
+
+  updateService(data) {
+    this.affiliation.updateService(this.position, data);
+    this.showSidebar = false;
+  }
+}
