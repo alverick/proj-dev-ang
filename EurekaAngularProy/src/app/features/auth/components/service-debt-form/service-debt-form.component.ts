ae9da8af0a290340e5e076bb8212e21f7dd2e6c8
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-11-14 17:42:08 -0500

    CSBIE-271 fix(auth): debt form validations fixed

--- EurekaAngularProy/src/app/features/auth/components/service-debt-form/service-debt-form.component.ts
+++ EurekaAngularProy/src/app/features/auth/components/service-debt-form/service-debt-form.component.ts
@@ -1,5 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
+import { pathEq } from 'ramda';
 import { IErrorMessages } from '../../../../shared/models/forms';
 
 @Component({
@@ -7,13 +16,18 @@ import { IErrorMessages } from '../../../../shared/models/forms';
   templateUrl: './service-debt-form.component.html',
   styleUrls: ['./service-debt-form.component.scss'],
 })
-export class ServiceDebtFormComponent implements OnInit {
+export class ServiceDebtFormComponent
+  implements OnInit, OnChanges, AfterViewInit
+{
   @Input() form: FormGroup;
   @Input() errorMessages: IErrorMessages;
   @Input() paymentTypeOptions: any[];
   @Input() currencyOptions: any[];
   @Input() chargeTypeOptions: any[];
   @Input() interestTypeOptions: any[];
+  @Input() submitted = false;
+  @ViewChild('formElm', { static: false })
+  htmlForm: NgForm;
   showArrearsFields = false;
   unitAmount = 'S/';
 
@@ -21,17 +35,40 @@ export class ServiceDebtFormComponent implements OnInit {
 
   ngOnInit() {
     const { chargeInterest, interestType } = this.form.value;
-    this.showArrearsFields = chargeInterest === 'S';
+    this.processArrearsMode(chargeInterest === 'S');
     this.unitAmount = interestType === 'M' ? 'S/' : '%';
     this.listenForms();
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (pathEq(['submitted', 'currentValue'], true, changes) && this.htmlForm) {
+      this.htmlForm.onSubmit(null);
+    }
+  }
+
+  processArrearsMode(show: boolean) {
+    this.showArrearsFields = show;
+    ['chargeType', 'interestType', 'amount'].forEach((field) => {
+      if (show) {
+        this.form.get(field).enable();
+      } else {
+        this.form.get(field).disable();
+      }
+    });
+  }
+
   listenForms() {
     this.form.get('chargeInterest').valueChanges.subscribe((val) => {
-      this.showArrearsFields = val === 'S';
+      this.processArrearsMode(val === 'S');
     });
     this.form.get('interestType').valueChanges.subscribe((val) => {
       this.unitAmount = val === 'M' ? 'S/' : '%';
     });
+  }
+
+  ngAfterViewInit(): void {
+    if (this.submitted && this.htmlForm) {
+      this.htmlForm.onSubmit(null);
+    }
   }
 }
