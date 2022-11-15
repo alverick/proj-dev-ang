08d14c4f9962945e9aba93e52dfc4f687cd81cab
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-11-15 16:54:48 -0500

    CSBIE-271 fix(general): update prefix for directive only numbers in form

--- EurekaAngularProy/src/app/shared/directives/only-numbers-form.directive.ts
+++ EurekaAngularProy/src/app/shared/directives/only-numbers-form.directive.ts
@@ -1,16 +1,13 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appOnlyNumbersForm]',
+  selector: '[csOnlyNumbersForm]',
 })
 export class OnlyNumbersFormDirective {
-  constructor(private _el: ElementRef) {
-    console.log('-> _el', _el);
-  }
+  constructor(private _el: ElementRef) {}
 
   @HostListener('input', ['$event'])
   onInputKeydow(event) {
-    console.log('-> event', event, this);
     const initalValue = this._el.nativeElement.value;
     this._el.nativeElement.value = initalValue.replace(/[^0-9\.]*/g, '');
     if (initalValue !== this._el.nativeElement.value) {
