fb0f68f85de8328c2f172c8e65402f71315f16a5
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-12-13 12:20:12 -0500

    CSBIE-270 fix(auth): added new routes

--- EurekaAngularProy/src/app/features/internal/internal-routing.module.ts
+++ EurekaAngularProy/src/app/features/internal/internal-routing.module.ts
@@ -10,10 +10,18 @@ import { GtpOutputGuard } from '../../shared/guards/gtp-output.guard';
 import { InternalAuthComponent } from './components/internal-auth/internal-auth.component';
 import {
   internalAuthRoutingNames,
+  internalRoutingChildNames,
   internalRoutingNames,
 } from './internal-routing.names';
 import { InternalComponent } from './internal.component';
-import { CompanyConfigurationPage } from './pages';
+import {
+  CompanyConfigurationPage,
+  CompanyServicesPage,
+  ServicesMainPage,
+  ServiceAddPage,
+  ServiceConfigurationPage,
+  ServiceInfoPage,
+} from './pages';
 import { ConfiguraCobrosParteCuatroComponent } from './pages/configura-cobros-parte-cuatro/configura-cobros-parte-cuatro.component';
 import { ConfiguraCobrosParteDosComponent } from './pages/configura-cobros-parte-dos/configura-cobros-parte-dos.component';
 import { ConfiguraCobrosParteTresComponent } from './pages/configura-cobros-parte-tres/configura-cobros-parte-tres.component';
@@ -22,8 +30,12 @@ import { EditarCobrosComponent } from './pages/editar-cobros/editar-cobros.compo
 import { HelpPage } from './pages/help/help.page';
 import { HomePage } from './pages/home/home.page';
 import { ResumenCobrosComponent } from './pages/resumen-cobros/resumen-cobros.component';
-import { CompanyDataResolver } from './resolvers/company-data.resolver';
-import { CompanyEntriesResolver } from './resolvers/company-entries.resolver';
+import {
+  CompanyAccountsResolver,
+  CompanyDataResolver,
+  CompanyEntriesResolver,
+  CompanyServicesResolver,
+} from './resolvers';
 
 const routes: Routes = [
   {
@@ -48,6 +60,36 @@ const routes: Routes = [
         path: internalRoutingNames.CHARGES,
         component: ResumenCobrosComponent,
         data: { isEdit: true, affiliationFlow: false },
+      },
+      {
+        path: internalRoutingNames.SERVICES,
+        component: ServicesMainPage,
+        children: [
+          {
+            path: appRoutingNames.EMPTY,
+            component: CompanyServicesPage,
+            resolve: { services: CompanyServicesResolver },
+          },
+          {
+            path: internalRoutingChildNames.SERVICES_ADD,
+            component: ServiceAddPage,
+            children: [
+              {
+                path: appRoutingNames.EMPTY,
+                redirectTo: internalRoutingChildNames.SERVICES_ADD_INFO,
+              },
+              {
+                path: internalRoutingChildNames.SERVICES_ADD_INFO,
+                component: ServiceInfoPage,
+                resolve: { accounts: CompanyAccountsResolver },
+              },
+              {
+                path: internalRoutingChildNames.SERVICES_ADD_CONFIGURATION,
+                component: ServiceConfigurationPage,
+              },
+            ],
+          },
+        ],
       },
       {
         path: internalRoutingNames.CHARGES_EDIT,
