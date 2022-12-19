611e51e45d12128a9569005f2ea6cd260758613b
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2022-12-19 09:58:26 -0500

    CSBIE-396 fix(shared): added forms error

--- EurekaAngularProy/src/app/shared/constants/company-errors.ts
+++ EurekaAngularProy/src/app/shared/constants/company-errors.ts
@@ -76,6 +76,9 @@ export const errorServiceInformation: IErrorMessages = {
     alfabetico: 'El nombre debe tener por lo menos una letra',
     change: 'Ingresa otro nombre para el servicio',
   },
+  debtorCode: {
+    change: 'Ingresa otro código deudor',
+  },
   debtorCodeCustom: {
     change: 'Ingresa otro código deudor',
   },
