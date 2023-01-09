ee618ddaffcef6907d3cd65b8d213e1ff0737771
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-01-09 18:13:55 -0500

    CSBIE-51 fix(shared): fix dependencies

--- EurekaAngularProy/src/app/shared/data/debt-data.service.ts
+++ EurekaAngularProy/src/app/shared/data/debt-data.service.ts
@@ -1,10 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { QueryParams } from '@ngrx/data';
 import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Debt, DebtsPagedList } from 'src/app/entities/debts';
 import { environment } from '../../../environments/environment';
+import { Debts, DebtsPagedList } from '../models/debts';
 
 @Injectable()
 export class DebtDataService {
@@ -13,11 +12,11 @@ export class DebtDataService {
   deleteAll(ids): Observable<any> {
     const url = `${environment.END_POINT}/debt/deleteAll`;
     return this.http
-      .post<Debt>(url, { ids })
+      .post<Debts>(url, { ids })
       .pipe(catchError((error) => throwError(error)));
   }
 
-  getAll(params: QueryParams): Observable<DebtsPagedList> {
+  getAll(params: any): Observable<DebtsPagedList> {
     const url = `${environment.END_POINT}/debt`;
     return this.http
       .get<DebtsPagedList>(url, { params })
