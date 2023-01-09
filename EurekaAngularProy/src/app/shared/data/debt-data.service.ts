3cdce90daac53ced2e40fefafcfb197dda5bae37
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-01-09 13:38:06 -0500

    CSBIE-51 fix(shared): moved debt services to data services

--- EurekaAngularProy/src/app/shared/data/debt-data.service.ts
+++ EurekaAngularProy/src/app/shared/data/debt-data.service.ts
@@ -1,18 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { QueryParams } from '@ngrx/data';
 import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Debt, DebtsPagedList } from 'src/app/entities/debts';
 import { environment } from '../../../environments/environment';
-import { Debts } from '../models/debts';
 
 @Injectable()
-export class DebtService {
+export class DebtDataService {
   constructor(private http: HttpClient) {}
 
   deleteAll(ids): Observable<any> {
     const url = `${environment.END_POINT}/debt/deleteAll`;
     return this.http
-      .post<Debts>(url, { ids })
+      .post<Debt>(url, { ids })
+      .pipe(catchError((error) => throwError(error)));
+  }
+
+  getAll(params: QueryParams): Observable<DebtsPagedList> {
+    const url = `${environment.END_POINT}/debt`;
+    return this.http
+      .get<DebtsPagedList>(url, { params })
       .pipe(catchError((error) => throwError(error)));
   }
 }
