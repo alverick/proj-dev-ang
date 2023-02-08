39c1a151055e2bb091c7fd6d4bf9ed37895995fb
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-02-08 10:46:12 -0500

    CSBIE-212 chore(storybook): upgrade config storybook

--- EurekaAngularProy/.storybook/main.js
+++ EurekaAngularProy/.storybook/main.js
@@ -1,21 +1,29 @@
 const path = require('path');
 
 module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/angular',
   core: {
-    builder: 'webpack5',
+    builder: '@storybook/builder-webpack5',
   },
-  stories: ['../src/**/*.stories.[tj]s'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-knobs'],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
       use: [
-        'postcss-loader',
         {
           loader: 'sass-resources-loader',
           options: {
             resources: [
               path.resolve(__dirname, '../src/scss/_configuration.scss'),
+              path.resolve(
+                __dirname,
+                '../node_modules/include-media/dist/_include-media.scss'
+              ),
             ],
           },
         },
