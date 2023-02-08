6b41cf7efe3f26fe18a51792e9474d0ac418e49d
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-02-08 18:24:05 -0500

    DDCS-3 feat(internal): added dashboard card component

---
+++ EurekaAngularProy/src/app/features/internal/components/dashboard-card/dashboard-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'cs-dashboard-card',
+  templateUrl: './dashboard-card.component.html',
+  styleUrls: ['./dashboard-card.component.scss'],
+})
+export class DashboardCardComponent {
+  @Input() title: string;
+  @Input() currency: string;
+  @Input() amount: number;
+  @Input() link: number;
+  @Input() peopleQuantity: number;
+}
---
+++ EurekaAngularProy/src/app/features/internal/components/dashboard-card/dashboard-card.stories.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import type { Meta } from '@storybook/angular';
+import { moduleMetadata, Story } from '@storybook/angular';
+
+import { SharedModule } from '../../../../shared/shared.module';
+import { DashboardCardComponent } from './dashboard-card.component';
+
+export default {
+  title: 'Internal/Dashboard/Dashboard Card',
+  component: DashboardCardComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [],
+      imports: [
+        BrowserAnimationsModule,
+        HttpClientModule,
+        CommonModule,
+        SharedModule,
+      ],
+    }),
+  ],
+  argTypes: { sendForm: { action: 'clicked' } },
+} as Meta;
+
+const Template: Story<DashboardCardComponent> = (
+  args: DashboardCardComponent
+) => ({
+  props: args,
+});
+
+export const normal = Template.bind({});
+normal.args = {
+  title: 'Cobro total',
+  currency: 'S/',
+  amount: '100000.00',
+  peopleQuantity: '1234',
+  link: '#home',
+};
+
+export const empty = Template.bind({});
+empty.args = {
+  title: 'Cobro total',
+  currency: 'S/',
+  amount: '',
+  peopleQuantity: '0',
+  link: '#home',
+};
