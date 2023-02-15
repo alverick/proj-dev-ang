db7b09fe6d88bf4a4978ed48b3244786d14fec44
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-02-15 14:30:54 -0500

    DDCS-3 feat(internal): added dashboard page

---
+++ EurekaAngularProy/src/app/features/internal/pages/dashboard/dashboard.page.ts
@@ -0,0 +1,115 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { pathOr } from 'ramda';
+import { isNotNilOrEmpty } from 'ramda-adjunct';
+import { filter } from 'rxjs/operators';
+
+import {
+  CollectAmounts,
+  HistoricalData,
+  TopClients,
+} from '../../../../shared/data/dashboard-data.service';
+import { GraphData } from '../../components/dashboard-graph/dashboard-graph.component';
+import { DashboardService } from '../../services';
+
+const dateFormat: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+};
+
+export interface FilterParams {
+  service: number[];
+  dateType: string;
+  startDate: string;
+  endDate: string;
+}
+
+@Component({
+  selector: 'cs-dashboard',
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage implements OnInit {
+  optionsDates = [
+    { name: 'Pago', code: 'PaymentDate' },
+    { name: 'Emisión', code: 'EmissionDate' },
+    { name: 'Vencimiento', code: 'DueDate' },
+  ];
+  dateTo = new Date();
+  dateFrom = new Date();
+  initialValue;
+  services: any;
+  collectAmounts: CollectAmounts;
+  clients: TopClients[] = [];
+  graphData: GraphData;
+  @ViewChild('outputHtml', { static: false }) outputHtml: ElementRef;
+
+  constructor(private dashboard: DashboardService) {
+    this.dateFrom.setMonth(this.dateFrom.getMonth() - 1);
+    console.log(this.dateFrom);
+  }
+
+  ngOnInit() {
+    this.dashboard
+      .getServices()
+      .pipe(filter((value) => isNotNilOrEmpty(value)))
+      .subscribe((value) => {
+        console.log('getServices', value);
+        const servicesList = value.map(({ currency, id, name }) => ({
+          id,
+          name,
+          currency,
+        }));
+        const servicesListId = value.map(({ id }) => id);
+        this.services = servicesList;
+        this.initialValue = {
+          payment: this.optionsDates[0],
+          dateTo: this.dateTo,
+          dateFrom: this.dateFrom,
+          services: servicesList,
+        };
+        console.log('initialValue', this.initialValue);
+        this.query({
+          service: servicesListId,
+          dateType: 'PaymentDate',
+          startDate: this.dateFrom.toLocaleDateString('zh-TW', dateFormat),
+          endDate: this.dateTo.toLocaleDateString('zh-TW', dateFormat),
+        });
+      });
+    console.log('ngOnInit', this.services);
+  }
+
+  sendFilters(value: any) {
+    console.log(value);
+    this.query({
+      service: value.services.map(({ id }) => id),
+      dateType: value.payment.code,
+      startDate: value.dateFrom.toLocaleDateString('zh-TW', dateFormat),
+      endDate: value.dateTo.toLocaleDateString('zh-TW', dateFormat),
+    });
+  }
+
+  query(filterData: FilterParams) {
+    console.log('filterData', filterData);
+    this.dashboard.getCollect(filterData).subscribe((value) => {
+      console.log('getCollect', value);
+      this.collectAmounts = value[0];
+    });
+    this.dashboard.getClients(filterData).subscribe((value) => {
+      console.log('getClients', value);
+      this.clients = value;
+    });
+    this.dashboard.getHistorical(filterData).subscribe((value) => {
+      const labels: string[] = pathOr([], [0, 'date'], value);
+      const datasets = value.map(({ amountCollected, service }) => ({
+        label: service,
+        data: amountCollected,
+      }));
+      console.log('getHistorical', value, labels, datasets);
+      this.graphData = {
+        labels: labels.map((item) => item.replace(' 00:00:00', '')),
+        datasets,
+      };
+    });
+  }
+}
