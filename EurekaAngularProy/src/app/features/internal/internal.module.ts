34266c8fd4b08d8e2475b6d873554e5bca905f44
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-02-20 14:40:01 -0500

    DDCS-3 feat(internal): added dashboard components to internal module

--- EurekaAngularProy/src/app/features/internal/internal.module.ts
+++ EurekaAngularProy/src/app/features/internal/internal.module.ts
@@ -2,8 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { SharedModule } from 'src/app/shared/shared.module';
+
 import { CompanyPasswordFormComponent } from './components/company-password-form/company-password-form.component';
 import { CompanyUpdateFormComponent } from './components/company-update-form/company-update-form.component';
+import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component';
+import { DashboardFilterComponent } from './components/dashboard-filter/dashboard-filter.component';
+import { DashboardGraphComponent } from './components/dashboard-graph/dashboard-graph.component';
+import { DashboardTableComponent } from './components/dashboard-table/dashboard-table.component';
 import { HelpItemComponent } from './components/help-item/help-item.component';
 import { InternalHeaderComponent } from './components/internal-header/internal-header.component';
 import { DateDirective } from './directives/date.directive';
@@ -11,8 +16,8 @@ import { DecimalesDirective } from './directives/decimales.directive';
 import { NumberDirectiveHome } from './directives/number.directive';
 import { PuntoDirective } from './directives/punto.directive';
 import { ShearDirective } from './directives/shear.directive';
-import { InternalRoutingModule } from './internal-routing.module';
 import { InternalComponent } from './internal.component';
+import { InternalRoutingModule } from './internal-routing.module';
 import { PAGES } from './pages';
 import { AgregaCobroComponent } from './pages/home/components/agrega-cobro.component';
 import { DebtComponent } from './pages/home/components/debt.component';
@@ -38,6 +43,10 @@ import { SERVICES } from './services';
     CompanyPasswordFormComponent,
     ...PAGES,
     TableMovementsComponent,
+    DashboardCardComponent,
+    DashboardFilterComponent,
+    DashboardTableComponent,
+    DashboardGraphComponent,
   ],
   imports: [
     CommonModule,
