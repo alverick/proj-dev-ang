eaee3b73d2c6bf15455235a01c2127655b822b44
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-02-20 14:41:22 -0500

    DDCS-3 feat(shared): added dashboard data service to internal module

--- EurekaAngularProy/src/app/shared/data/index.ts
+++ EurekaAngularProy/src/app/shared/data/index.ts
@@ -1,5 +1,7 @@
+import { DashboardDataService } from './dashboard-data.service';
 import { DebtDataService } from './debt-data.service';
 
-export const DATA_SERVICES = [DebtDataService];
+export const DATA_SERVICES = [DebtDataService, DashboardDataService];
 
+export { DashboardDataService } from './dashboard-data.service';
 export { DebtDataService } from './debt-data.service';
