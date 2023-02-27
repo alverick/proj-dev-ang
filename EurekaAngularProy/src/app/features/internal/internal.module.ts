09a3a66b102db187403734cf977aa4eb527dd848
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-02-27 11:30:50 -0500

    DDCS-8 fix(internal): added new component Header Icon

--- EurekaAngularProy/src/app/features/internal/internal.module.ts
+++ EurekaAngularProy/src/app/features/internal/internal.module.ts
@@ -2,18 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { SharedModule } from 'src/app/shared/shared.module';
+
 import { CompanyPasswordFormComponent } from './components/company-password-form/company-password-form.component';
 import { CompanyUpdateFormComponent } from './components/company-update-form/company-update-form.component';
-import { HelpItemComponent } from './components/help-item/help-item.component';
 import { InternalHeaderComponent } from './components/internal-header/internal-header.component';
 import { DateDirective } from './directives/date.directive';
 import { DecimalesDirective } from './directives/decimales.directive';
 import { NumberDirectiveHome } from './directives/number.directive';
 import { PuntoDirective } from './directives/punto.directive';
 import { ShearDirective } from './directives/shear.directive';
-import { InternalRoutingModule } from './internal-routing.module';
 import { InternalComponent } from './internal.component';
+import { InternalRoutingModule } from './internal-routing.module';
 import { PAGES } from './pages';
+import { HelpHeaderIconComponent } from './pages/help/help-header-icon/help-header-icon.component';
 import { AgregaCobroComponent } from './pages/home/components/agrega-cobro.component';
 import { DebtComponent } from './pages/home/components/debt.component';
 import { PaymentDetailComponent } from './pages/home/components/payment-detail/payment-detail.component';
@@ -33,11 +34,11 @@ import { SERVICES } from './services';
     ShearDirective,
     InternalHeaderComponent,
     PaymentDetailComponent,
-    HelpItemComponent,
     CompanyUpdateFormComponent,
     CompanyPasswordFormComponent,
     ...PAGES,
     TableMovementsComponent,
+    HelpHeaderIconComponent,
   ],
   imports: [
     CommonModule,
