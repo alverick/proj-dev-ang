25a35a02088d3a2753d3cc9090d03bd06512d914
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-02-27 09:58:28 -0500

    DDCS-8 fix(primeng): config lang for primeng

--- EurekaAngularProy/src/app/app.component.ts
+++ EurekaAngularProy/src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
 import { LoginService } from 'src/app/shared/services/login.service';
 import { environment } from 'src/environments/environment';
+
 import { appFullRoutingNames } from './app-routing.names';
 import {
   authFullRoutingChildNames,
   authFullRoutingNames,
 } from './features/auth/auth-routing.names';
+import { primeng } from './shared/lang/es';
 import { GoogleAnalytics } from './shared/services/googleAnalytics.service';
 
 declare let fbq: (...args: any[]) => void;
@@ -18,14 +21,15 @@ declare let fbq: (...args: any[]) => void;
   styleUrls: ['./app.component.css'],
   providers: [LoginService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Cobro Simple – Interbank';
 
   constructor(
     private router: Router,
     matIconRegistry: MatIconRegistry,
     domSanitizer: DomSanitizer,
-    private gaService: GoogleAnalytics
+    private gaService: GoogleAnalytics,
+    private config: PrimeNGConfig
   ) {
     this.router.events.subscribe((e) => {
       if (e instanceof NavigationEnd) {
@@ -79,6 +83,10 @@ export class AppComponent {
       domSanitizer.bypassSecurityTrustResourceUrl('/assets/images/new-tab.svg'),
       { viewBox: '0 0 24 24' }
     );
+  }
+
+  ngOnInit() {
+    this.config.setTranslation(primeng);
   }
 
   private sendTrackPageViewPixel(pathComponent: string): void {
