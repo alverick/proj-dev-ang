d3a1b472f23be7d801fc1ec557d56dc2cac0989f
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-03-07 16:51:28 -0500

    DDCS-8 fix(internal): fix position default

--- EurekaAngularProy/src/app/features/internal/pages/services-main/services-main.page.ts
+++ EurekaAngularProy/src/app/features/internal/pages/services-main/services-main.page.ts
@@ -11,20 +11,20 @@ import {
   styleUrls: ['./services-main.page.scss'],
 })
 export class ServicesMainPage {
-  position = 0;
+  position = 2;
   constructor(private router: Router) {
     router.events.subscribe((val: any) => {
       if (val instanceof NavigationEnd) {
         switch (val.url) {
-          case internalFullRoutingNames.SERVICES:
-            this.position = 2;
+          case internalFullRoutingChildNames.SERVICES_ADD_INFO:
+            this.position = 0;
             break;
           case internalFullRoutingChildNames.SERVICES_ADD_CONFIGURATION:
             this.position = 1;
             break;
-          case internalFullRoutingChildNames.SERVICES_ADD_INFO:
+          case internalFullRoutingNames.SERVICES:
           default:
-            this.position = 0;
+            this.position = 2;
             break;
         }
       }
