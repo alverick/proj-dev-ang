7fd3a662122f4aa15ce6c1a8bc2423bfda9994a2
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-03-30 16:14:28 -0500

    DDCS-6 fix(public): renamed landing

--- EurekaAngularProy/src/app/features/public/public-routing.module.ts
+++ EurekaAngularProy/src/app/features/public/public-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { appRoutingNames } from 'src/app/app-routing.collection';
 import { LogoutGuard } from '../../shared/guards/logout.guard';
 import { AfiliacionComponent } from './pages/afiliacion/afiliacion.component';
-import { LandingComponent } from './pages/landing/landing.component';
+import { LandingPage } from './pages/landing/landing.page';
 import { publicRoutingNames } from './public-routing.names';
 
 const routes: Routes = [
   {
     path: appRoutingNames.EMPTY,
-    component: LandingComponent,
+    component: LandingPage,
     canActivate: [LogoutGuard],
   },
   {
--- EurekaAngularProy/src/app/features/public/public.module.ts
+++ EurekaAngularProy/src/app/features/public/public.module.ts
@@ -4,15 +4,11 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 import { AfiliacionComponent } from './pages/afiliacion/afiliacion.component';
 import { LandingCarouselComponent } from './pages/landing/components/landing-carousel/landing-carousel.component';
-import { LandingComponent } from './pages/landing/landing.component';
+import { LandingPage } from './pages/landing/landing.page';
 import { PublicRoutingModule } from './public-routing.module';
 
 @NgModule({
-  declarations: [
-    LandingComponent,
-    AfiliacionComponent,
-    LandingCarouselComponent,
-  ],
+  declarations: [LandingPage, AfiliacionComponent, LandingCarouselComponent],
   imports: [CommonModule, PublicRoutingModule, SharedModule, NgOptimizedImage],
 })
 export class PublicModule {}
