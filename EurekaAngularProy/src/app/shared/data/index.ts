e7a3e93c372d000c4a4e24df7106d23fd84ab709
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-05-23 11:49:34 -0500

    DDCS-1192 feat(shared): added data services

--- EurekaAngularProy/src/app/shared/data/index.ts
+++ EurekaAngularProy/src/app/shared/data/index.ts
@@ -1,7 +1,13 @@
 import { DashboardDataService } from './dashboard-data.service';
 import { DebtDataService } from './debt-data.service';
+import { QueryDataService } from './query-data.service';
 
-export const DATA_SERVICES = [DebtDataService, DashboardDataService];
+export const DATA_SERVICES = [
+  DebtDataService,
+  DashboardDataService,
+  QueryDataService,
+];
 
 export { DashboardDataService } from './dashboard-data.service';
 export { DebtDataService } from './debt-data.service';
+export { QueryDataService } from './query-data.service';
---
+++ EurekaAngularProy/src/app/shared/data/query-data.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { environment } from '../../../environments/environment';
+
+export interface MessageResult {
+  success: boolean;
+  code?: number;
+  message?: string;
+  id?: number;
+}
+
+@Injectable()
+export class QueryDataService {
+  constructor(private http: HttpClient) {}
+
+  regularizeAll(): Observable<MessageResult> {
+    const url = `${environment.END_POINT}/Query/regularizeStateProcessAll`;
+    return this.http
+      .post<MessageResult>(url, {})
+      .pipe(catchError((error) => throwError(error)));
+  }
+
+  regularizeProcessById(processId: number): Observable<MessageResult> {
+    const url = `${environment.END_POINT}/Query/regularizeStateProcessById/${processId}`;
+    return this.http
+      .post<MessageResult>(url, {})
+      .pipe(catchError((error) => throwError(error)));
+  }
+}
