29cf410765b0ec06bed7130ae8dd0aa786765c64
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-06-12 12:02:07 -0500

    DDCS-1295 fix(shared): update story to CSF3

--- EurekaAngularProy/src/app/shared/components/label-control/label-control.stories.ts
+++ EurekaAngularProy/src/app/shared/components/label-control/label-control.stories.ts
@@ -1,92 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 
 import { SharedModule } from '../../shared.module';
 import { LabelControlComponent } from './label-control.component';
 
-export default {
+const meta: Meta<LabelControlComponent> = {
   title: 'UI/Forms/Label',
+  component: LabelControlComponent,
   decorators: [
     moduleMetadata({
       declarations: [],
-      imports: [
-        BrowserAnimationsModule,
-        HttpClientModule,
-        CommonModule,
-        SharedModule,
-      ],
+      imports: [BrowserAnimationsModule, CommonModule, SharedModule],
     }),
   ],
-} as Meta;
+};
 
-const Template: Story<LabelControlComponent> = (
-  args: LabelControlComponent
-) => ({
-  props: args,
-  template: `<cs-label-control class="tw-m-5">Label <input
-      type="text"
-      pInputText
-    /></cs-label-control>`,
-});
+export default meta;
 
-export const normal = Template.bind({});
-normal.args = {};
+type Story = StoryObj<LabelControlComponent>;
 
-// export const DefaultStory = () => ({
-//   template: `<cs-label-control class="tw-m-5">Label <input
-//       type="text"
-//       pInputText
-//     /></cs-label-control>`,
-//   props: {
-//     text: 'Button with custom styles',
-//   },
-//   styles: [
-//     `
-//       cs-text-input {
-//         margin: 25px;
-//       }
-//     `,
-//   ],
-// });
-//
-// DefaultStory.story = {
-//   name: 'Default',
-// };
-//
-// export const normal = () => ({
-//   component: LabelControlComponent,
-//   props: {
-//     gtpMode: boolean('GTP Mode', false),
-//   },
-//   argTypes: { sendForm: { action: 'clicked' } },
-// });
-//
-// export const ActionOnly = () => ({
-//   component: Button,
-//   props: {
-//     text: 'Action only',
-//     onClick: action('log 1'),
-//   },
-// });
-//
-// ActionOnly.story = {
-//   name: 'Action only',
-// };
-//
-// export const ActionAndMethod = () => ({
-//   component: Button,
-//   props: {
-//     text: 'Action and Method',
-//     onClick: (e) => {
-//       console.log(e);
-//       e.preventDefault();
-//       action('log2')(e.target);
-//     },
-//   },
-// });
-//
-// ActionAndMethod.story = {
-//   name: 'Action and method',
-// };
+export const Default: Story = {
+  render: () => ({
+    template: `<cs-label-control class="tw-m-5"
+      >Label 1 <input type="text" pInputText />
+    </cs-label-control>`,
+  }),
+};
+
+export const WithHint: Story = {
+  render: (args) => ({
+    template: `<cs-label-control class="tw-m-5"
+      >Label 1 <input type="text" pInputText />
+      <div hint class="tw-text-error">
+        El nuevo nombre 1 <strong> ${args.onlyControl} </strong> esta siendo procesado
+      </div>
+    </cs-label-control>`,
+  }),
+  args: {
+    onlyControl: true,
+  },
+};
