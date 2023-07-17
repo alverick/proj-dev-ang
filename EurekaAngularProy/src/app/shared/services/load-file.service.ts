21cd6f6162ff8b8c687f254171af9f3d18698256
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-07-17 14:05:26 -0500

    DDCS-1192 refactor(shared): verify status refactoring setTimeout

--- EurekaAngularProy/src/app/shared/services/load-file.service.ts
+++ EurekaAngularProy/src/app/shared/services/load-file.service.ts
@@ -4,21 +4,35 @@ import {
   Injectable,
   ViewContainerRef,
 } from '@angular/core';
-import { drawPopup } from 'src/app/shared/utils/helpers/popups';
-import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { repeat, takeUntil } from 'rxjs/operators';
+
+import { internalFullRoutingNames } from '../../app-routing.collection';
 import { LoadFileComponent } from '../components/load-file/load-file.component';
-import { ExcelService } from './excel.service';
-import { GoogleAnalytics } from './googleAnalytics.service';
+import { ExcelService, ProcessStatus } from './excel.service';
+import { StorageService } from './storage.service';
+
+export interface ModalCloseData {
+  status: string;
+  rowsAccepted: number;
+  rowsRejected: number;
+  dataType: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoadFileService {
-  constructor(private excelService: ExcelService) {}
+  constructor(
+    private excelService: ExcelService,
+    private storageService: StorageService,
+    private router: Router
+  ) {}
 
   private componentRef: ComponentRef<LoadFileComponent> = null;
   private cancel = true;
-  public onClose = new EventEmitter<any>();
+  public onClose = new EventEmitter<Partial<ModalCloseData>>();
 
   public verify(container: ViewContainerRef) {
     container.clear();
@@ -47,65 +61,70 @@ export class LoadFileService {
   }
 
   private verifyStatus() {
+    const stop$ = new Subject();
     this.cancel = false;
-    let recursiveFunc = (value) => {
-      if (this.cancel) return;
-      if (value.status === 'REJECTED') {
-        this.excelService.errores = value.errors;
-        this.componentRef.destroy();
-        this.onClose.emit({
-          status: 'rejected',
-          rowsAccepted: value.rowsUploaded,
-          rowsRejected: value.rowsRejected,
-        });
-      } else if (value.status === 'COMPLETED') {
-        this.gaService.sendEvent('CargarExcel', {
-          event_category: 'CargaExcel',
-          event_label: 'cargar_excel',
-        });
-        this.excelService.statusUpload = false;
-        this.excelService.errores = [];
-        this.componentRef.destroy();
-        let msg = '';
-        if (this.excelService.service.dataType === 'C') {
-          // msg = `Se cargaron ${value.rowsUploaded} registros`;
-          msg = `¡Listo! Se agregaron nuevas deudas `;
-        } else {
-          msg = `¡Listo! Se agregaron nuevos clientes`;
-        }
-        Swal.fire({
-          title: msg,
-          text: 'Recuerda que puedes eliminar y/o editar los datos de tus clientes desde la página de movimientos',
-          showCloseButton: true,
-          onOpen: drawPopup,
-          confirmButtonText: 'CERRAR',
-          onAfterClose: () => {
-            this.onClose.emit({ status: 'completed' });
-          },
-        });
-      } else {
-        this.componentRef.instance.progress.mode = 'determinate';
-        this.componentRef.instance.progress.value = value.advance;
-        if (value.status == 'VALIDATING') {
-          this.componentRef.instance.progress.status = `Validando (${value.phase}/3)`;
-        } else if (value.status == 'SAVING') {
-          this.componentRef.instance.progress.status = `Grabando (${value.phase}/2)`;
-        }
-        var th = this;
-        setTimeout(() => {
-          th.excelService
-            .StatusExcel(th.excelService.idProcess)
-            .subscribe(recursiveFunc);
-        }, 2000);
-      }
+
+    const delayBy = 2000;
+    const stopLoop = () => {
+      stop$.next(true);
+      stop$.complete();
     };
+    this.excelService
+      .StatusExcel(this.excelService.idProcess)
+      .pipe(repeat({ delay: delayBy }), takeUntil(stop$))
+      .subscribe(
+        ({
+          advance,
+          errors,
+          phase,
+          rowsRejected,
+          rowsUploaded,
+          status,
+        }: ProcessStatus) => {
+          if (
+            this.cancel ||
+            this.router.url !== internalFullRoutingNames.HOME
+          ) {
+            stopLoop();
+            return;
+          }
+          if (status === 'REJECTED') {
+            stopLoop();
+            this.componentRef.destroy();
+            this.excelService.errores = errors;
+            this.onClose.emit({
+              status: 'rejected',
+              rowsAccepted: rowsUploaded,
+              rowsRejected: rowsRejected,
+            });
+          } else if (status === 'FAILED') {
+            stopLoop();
+            this.componentRef.destroy();
+            this.onClose.emit({
+              status: 'failed',
+            });
+          } else if (status === 'COMPLETED') {
+            stopLoop();
+            this.componentRef.destroy();
+            this.excelService.statusUpload = false;
+            this.excelService.errores = [];
+            this.onClose.emit({
+              status: 'completed',
+              dataType: this.excelService.service.dataType,
+            });
+          } else {
+            this.componentRef.instance.progress.mode = 'determinate';
+            this.componentRef.instance.progress.value = advance;
+            if (status == 'VALIDATING') {
+              this.componentRef.instance.progress.status = `Validando (${phase}/3)`;
+            } else if (status == 'SAVING') {
+              this.componentRef.instance.progress.status = `Grabando (${phase}/2)`;
+            }
+          }
+        }
+      );
     this.componentRef.instance.progress.mode = 'determinate';
     this.componentRef.instance.progress.value = 0;
     this.componentRef.instance.progress.status = 'Validando (0/3)';
-    setTimeout(() => {
-      this.excelService
-        .StatusExcel(this.excelService.idProcess)
-        .subscribe(recursiveFunc);
-    }, 2000);
   }
 }
