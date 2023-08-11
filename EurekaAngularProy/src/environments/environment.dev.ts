e62c95eaab0c83939421dd81f9387938da8f6614
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-08-11 15:32:50 -0500

    DDCS-1601 feature(environment): added adobe property

--- EurekaAngularProy/src/environments/environment.dev.ts
+++ EurekaAngularProy/src/environments/environment.dev.ts
@@ -9,4 +9,6 @@ export const environment: IEnvironment = {
   END_POINT: serverUrl.dev,
   logLevel: NgxLoggerLevel.OFF,
   serverLogLevel: NgxLoggerLevel.ERROR,
+  adobe:
+    'https://assets.adobedtm.com/df637a308f4c/ec7ac20e39e1/launch-ed1665d4f083-development.min.js',
 };
