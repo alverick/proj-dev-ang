4154054979db3c7ae1e7bafe42f31b91f0b13af1
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-08-28 16:09:36 -0500

    DDCS-1636 fix(auth): moved stepper component

--- EurekaAngularProy/src/app/features/auth/pages/service-add/service-add.page.ts
+++ EurekaAngularProy/src/app/features/auth/pages/service-add/service-add.page.ts
@@ -1,5 +1,7 @@
 import { Component, HostListener } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationEnd, Router, RouterEvent, Scroll } from '@angular/router';
+import { map } from 'rxjs/operators';
+
 import { authFullRoutingChildNames } from '../../auth-routing.names';
 import { AffiliationService } from '../../services';
 
@@ -10,24 +12,27 @@ import { AffiliationService } from '../../services';
 })
 export class ServiceAddPage {
   position = 0;
-  steps = [{ title: 'Step 1' }, { title: 'Step 2' }, { title: 'Step 3' }];
+  steps = [{ title: 'Step 1' }, { title: 'Step 2' }];
+
   constructor(private router: Router, public affiliation: AffiliationService) {
-    router.events.subscribe((val: any) => {
-      if (val instanceof NavigationEnd) {
-        switch (val.url) {
-          case authFullRoutingChildNames.SERVICES_ADD_CONFIGURATION:
-            this.position = 1;
-            break;
-          case authFullRoutingChildNames.SERVICES_ADD_LIST:
-            this.position = 2;
-            break;
-          case authFullRoutingChildNames.SERVICES_ADD_INFO:
-          default:
-            this.position = 0;
-            break;
+    router.events
+      .pipe(map((evt) => (evt instanceof Scroll ? evt.routerEvent : evt)))
+      .subscribe((val: RouterEvent) => {
+        if (val instanceof NavigationEnd) {
+          switch (val.url) {
+            case authFullRoutingChildNames.SERVICES_ADD_CONFIGURATION:
+              this.position = 1;
+              break;
+            case authFullRoutingChildNames.SERVICES_ADD_INFO:
+              this.position = 0;
+              break;
+            case authFullRoutingChildNames.SERVICES_ADD_LIST:
+            default:
+              this.position = 2;
+              break;
+          }
         }
-      }
-    });
+      });
   }
 
   @HostListener('window:beforeunload', ['$event'])
