dee6d051bc0b0837de4479366cd2f969adcd9075
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-08-29 09:53:54 -0500

    DDCS-1636 fix(internal): fixed router events

--- EurekaAngularProy/src/app/features/internal/pages/services-main/services-main.page.ts
+++ EurekaAngularProy/src/app/features/internal/pages/services-main/services-main.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationEnd, Router, RouterEvent, Scroll } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 import {
   internalFullRoutingChildNames,
@@ -13,22 +14,24 @@ import {
 })
 export class ServicesMainPage {
   position = 2;
-  constructor(private router: Router) {
-    router.events.subscribe((val: any) => {
-      if (val instanceof NavigationEnd) {
-        switch (val.url) {
-          case internalFullRoutingChildNames.SERVICES_ADD_INFO:
-            this.position = 0;
-            break;
-          case internalFullRoutingChildNames.SERVICES_ADD_CONFIGURATION:
-            this.position = 1;
-            break;
-          case internalFullRoutingNames.SERVICES:
-          default:
-            this.position = 2;
-            break;
+  constructor(protected router: Router) {
+    router.events
+      .pipe(map((evt) => (evt instanceof Scroll ? evt.routerEvent : evt)))
+      .subscribe((val: RouterEvent) => {
+        if (val instanceof NavigationEnd) {
+          switch (val.url) {
+            case internalFullRoutingChildNames.SERVICES_ADD_INFO:
+              this.position = 0;
+              break;
+            case internalFullRoutingChildNames.SERVICES_ADD_CONFIGURATION:
+              this.position = 1;
+              break;
+            case internalFullRoutingNames.SERVICES:
+            default:
+              this.position = 2;
+              break;
+          }
         }
-      }
-    });
+      });
   }
 }
