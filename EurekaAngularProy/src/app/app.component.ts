503c16793393dc390b09fcb6308f1c72b7374fcd
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-08-31 18:25:37 -0500

    DDCS-1601 fix(app): removes ga and fb tracking

--- EurekaAngularProy/src/app/app.component.ts
+++ EurekaAngularProy/src/app/app.component.ts
@@ -4,18 +4,11 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 import { LoginService } from 'src/app/shared/services/login.service';
-import { environment } from 'src/environments/environment';
 
 import { appFullRoutingNames } from './app-routing.names';
-import {
-  authFullRoutingChildNames,
-  authFullRoutingNames,
-} from './features/auth/auth-routing.names';
 import { primeng } from './shared/lang/es';
 import { AdobeAnalyticsService } from './shared/services/adobe-analytics.service';
-import { GoogleAnalytics } from './shared/services/googleAnalytics.service';
 
-declare let fbq: (...args: any[]) => void;
 @Component({
   selector: 'cs-root',
   templateUrl: './app.component.html',
@@ -30,7 +23,6 @@ export class AppComponent implements OnInit {
     private router: Router,
     matIconRegistry: MatIconRegistry,
     domSanitizer: DomSanitizer,
-    private gaService: GoogleAnalytics,
     private primengConfig: PrimeNGConfig,
     private adobeAnalytics: AdobeAnalyticsService
   ) {
@@ -78,39 +70,5 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.primengConfig.ripple = true;
     this.primengConfig.setTranslation(primeng);
-  }
-
-  private sendTrackPageViewPixel(pathComponent: string): void {
-    switch (pathComponent) {
-      case appFullRoutingNames.LANDING:
-        fbq('track', 'PageView');
-        break;
-      case authFullRoutingNames.COMPANY_REGISTER:
-        fbq('track', 'PageView');
-        break;
-      case authFullRoutingNames.COMPANY_FILL_DATA:
-        fbq('track', 'PageView');
-        break;
-      case authFullRoutingNames.COMPANY_FINISHED:
-        fbq('track', 'PageView');
-        break;
-      case authFullRoutingChildNames.SERVICES_ADD_INFO:
-        fbq('track', 'PageView');
-        break;
-      case authFullRoutingChildNames.SERVICES_ADD_CONFIGURATION:
-        fbq('track', 'PageView');
-        break;
-      case authFullRoutingChildNames.SERVICES_ADD_LIST:
-        fbq('track', 'PageView');
-        break;
-      case authFullRoutingNames.REGISTRATION_FINISHED:
-        fbq('track', 'Contact', {
-          content_name: 'cobro-simple-5',
-        });
-        break;
-      /*default:
-        fbq('track', 'PageView');
-        break;*/
-    }
   }
 }
