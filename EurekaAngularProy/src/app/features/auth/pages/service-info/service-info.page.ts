1fb362b962efde950fec072765aa83ede8c3fcbf
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-08-31 18:35:50 -0500

    DDCS-1601 fix(auth): added adobe tracking

--- EurekaAngularProy/src/app/features/auth/pages/service-info/service-info.page.ts
+++ EurekaAngularProy/src/app/features/auth/pages/service-info/service-info.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { ServicesFormsService } from '../../../../shared/services';
+import {
+  AdobeAnalyticsService,
+  AdobeEvent,
+} from '../../../../shared/services/adobe-analytics.service';
 import { authFullRoutingChildNames } from '../../auth-routing.names';
 import { errorServiceInformation } from '../../constants';
 import { AffiliationService } from '../../services';
@@ -16,7 +21,8 @@ export class ServiceInfoPage implements OnInit {
   constructor(
     private router: Router,
     public affiliation: AffiliationService,
-    private serviceForms: ServicesFormsService
+    private serviceForms: ServicesFormsService,
+    private adobeAnalytics: AdobeAnalyticsService
   ) {}
 
   ngOnInit() {
@@ -25,13 +31,33 @@ export class ServiceInfoPage implements OnInit {
     });
   }
   onSubmit() {
-    this.router.navigate([
+    const { useAgent } = this.affiliation.serviceForm.value;
+    this.adobeAnalytics.trackEvent(AdobeEvent.trackFormSubmit, {
+      category: 'Registrate – Información de servicio',
+      action: 'Click',
+      label: 'Siguiente',
+      module: 'Home',
+      location: 'Registrate',
+      step: 'Step3',
+      state: 'Envío exitoso',
+      metadata: [
+        {
+          key: 'Canales Digitales',
+          value: 'true',
+        },
+        {
+          key: 'Agentes',
+          value: useAgent ? 'true' : 'false',
+        },
+      ],
+    });
+    void this.router.navigate([
       authFullRoutingChildNames.SERVICES_ADD_CONFIGURATION,
     ]);
   }
 
   onCancel() {
     this.serviceForms.resetServicesForms();
-    this.router.navigate([authFullRoutingChildNames.SERVICES_ADD_LIST]);
+    void this.router.navigate([authFullRoutingChildNames.SERVICES_ADD_LIST]);
   }
 }
