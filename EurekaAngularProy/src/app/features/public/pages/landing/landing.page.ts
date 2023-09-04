e3cd46a4e46582525a2b09eb69005c0ba1a66ee9
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-09-04 17:35:44 -0500

    DDCS-1636 fix(public): added landing images

--- EurekaAngularProy/src/app/features/public/pages/landing/landing.page.ts
+++ EurekaAngularProy/src/app/features/public/pages/landing/landing.page.ts
@@ -13,6 +13,7 @@ import { authFullRoutingNames } from '../../../auth/auth-routing.names';
 interface ItemLanding {
   title: string;
   content: string;
+  image?: string;
   position?: string;
 }
 
@@ -28,21 +29,25 @@ export class LandingPage implements OnDestroy {
   benefits: ItemLanding[] = [
     {
       title: 'Sin compartir número de cuenta',
+      image: 'icon-person-smile.svg',
       content:
         'Tus clientes podrán pagarte a través de "Pago de Servicios" en los canales digitales de Interbank y en más de 4,500 agentes.',
     },
     {
       title: 'Configura tus cobros',
+      image: 'icon-laptop-configuration.svg',
       content:
         'Elige el orden de pago de las deudas, si recibirás pagos por partes o incluirás el cobro de moras.',
     },
     {
       title: 'Cobranzas al instante',
+      image: 'icon-money-hand.svg',
       content:
         'Podrás identificar los pagos de cada cliente y ver el estado de pago en tiempo real.',
     },
     {
       title: 'Descarga reportes',
+      image: 'icon-laptop-report.svg',
       content:
         'Para facilitar tu proceso de conciliación y seguimiento de cobranzas.',
     },
@@ -79,16 +84,19 @@ export class LandingPage implements OnDestroy {
   payments: ItemLanding[] = [
     {
       title: 'Pagos parciales',
+      image: 'icon-money-cards.svg',
       content:
         'Configura si permitirás a tus clientes pagar su deuda total por partes',
     },
     {
       title: 'Pagos sucesivos',
+      image: 'icon-calendar.svg',
       content:
         'Podrás hacer que tus clientes paguen la deuda más antigua primero o la que ellos deseen',
     },
     {
       title: 'Cobro de mora',
+      image: 'icon-percent-more.svg',
       content:
         'Configura si deseas que tus clientes paguen una mora y define el importe de esta',
     },
@@ -138,6 +146,10 @@ export class LandingPage implements OnDestroy {
 
   sendAdobeTrack(action: Partial<ActionEventProperties>) {
     this.adobeAnalytics.trackEvent(AdobeEvent.trackAction, action);
+  }
+
+  trackByiItems(index: number) {
+    return index;
   }
 
   showModalTerms() {
