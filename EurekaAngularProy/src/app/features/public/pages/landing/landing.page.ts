3dee5eb3ebc2705b8ca9e35bdb7457095c4886fe
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-09-06 18:17:04 -0500

    DDCS-1636 fix(public): responsive fixes

--- EurekaAngularProy/src/app/features/public/pages/landing/landing.page.ts
+++ EurekaAngularProy/src/app/features/public/pages/landing/landing.page.ts
@@ -48,34 +48,30 @@ export class LandingPage implements OnInit, OnDestroy {
         'Para facilitar tu proceso de conciliación y seguimiento de cobranzas.',
     },
   ];
-  howWorks: ItemLanding[][] = [
-    [
-      {
-        title: 'Regístrate',
-        position: '01',
-        content: 'Crea tu contraseña y configura los cobros de tu empresa.',
-      },
-      {
-        title: 'Ingresa y carga* tu lista de clientes por cobrar',
-        position: '02',
-        content:
-          'Ingresa a la plataforma con tu RUC y contraseña, luego carga tu lista de clientes en la sección “agregar cobros”. <br /><br />*Sólo si eliges la opción de ingresar datos de tus clientes.',
-      },
-    ],
-    [
-      {
-        title: 'Empieza a recibir los pagos de tus clientes',
-        position: '03',
-        content:
-          'Ellos podrán pagarte buscando el nombre de tu empresa en el Interbank APP o Banca por Internet.',
-      },
-      {
-        title: 'Identifica y concilia los pagos recibidos',
-        position: '04',
-        content:
-          'Puedes ver en tiempo real desde la plataforma, quién te pagó y exportar reportes para analizar tus cobranzas.',
-      },
-    ],
+  howWorks: ItemLanding[] = [
+    {
+      title: 'Regístrate',
+      position: '01',
+      content: 'Crea tu contraseña y configura los cobros de tu empresa.',
+    },
+    {
+      title: 'Ingresa y carga* tu lista de clientes por cobrar',
+      position: '02',
+      content:
+        'Ingresa a la plataforma con tu RUC y contraseña, luego carga tu lista de clientes en la sección “agregar cobros”. <br /><br />*Sólo si eliges la opción de ingresar datos de tus clientes.',
+    },
+    {
+      title: 'Empieza a recibir los pagos de tus clientes',
+      position: '03',
+      content:
+        'Ellos podrán pagarte buscando el nombre de tu empresa en Interbank APP o Banca por Internet.',
+    },
+    {
+      title: 'Identifica y concilia los pagos recibidos',
+      position: '04',
+      content:
+        'Puedes ver en tiempo real desde la plataforma, quién te pagó y exportar reportes para analizar tus cobranzas.',
+    },
   ];
   payments: ItemLanding[] = [
     {
@@ -134,8 +130,8 @@ export class LandingPage implements OnInit, OnDestroy {
     });
   }
 
-  trackByiItems(index: number) {
-    return index;
+  trackByiItems(_index: number, item: ItemLanding): string {
+    return item.title;
   }
 
   showModalTerms() {
