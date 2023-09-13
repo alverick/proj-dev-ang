7d41bada3be96d6354e25ae1c346c7b509abcc96
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-09-13 09:05:07 -0500

    DDCS-2054 fix(general): removed methods and constructors empty

--- EurekaAngularProy/src/app/features/auth/pages/registration-update/registration-update.page.ts
+++ EurekaAngularProy/src/app/features/auth/pages/registration-update/registration-update.page.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'cs-registration-update',
   templateUrl: './registration-update.page.html',
   styleUrls: ['./registration-update.page.scss'],
 })
-export class RegistrationUpdatePage implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class RegistrationUpdatePage {}
