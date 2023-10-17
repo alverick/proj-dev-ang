a665f28c2887069d7d5fe679df1927eea6cd76f7
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-10-17 14:58:18 -0500

    DDCS-2502 fix(internal): blur content form

--- EurekaAngularProy/src/app/features/internal/pages/company-configuration/company-configuration.page.ts
+++ EurekaAngularProy/src/app/features/internal/pages/company-configuration/company-configuration.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import {
   documentTypes,
@@ -10,6 +11,8 @@ import {
   errorRegisterAuth,
   errorsRegisterForm,
 } from '../../../../shared/constants/company-errors';
+import { IEntryModel } from '../../../../shared/models';
+import { IDataEnterpriseModel } from '../../../../shared/models/data-enterprise.model';
 import { ModelFormGroup } from '../../../../shared/models/forms';
 import {
   AdobeAnalyticsService,
@@ -37,6 +40,7 @@ export class CompanyConfigurationPage implements OnInit {
     confirmNewPassword: errorRegisterAuth.passwordConfirm,
   };
   showSidebar = false;
+  blurContent = false;
 
   constructor(
     public companyConfiguration: CompanyConfigurationService,
@@ -45,7 +49,12 @@ export class CompanyConfigurationPage implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.activatedRoute.data.subscribe(({ company, entries }: any) => {
+    (
+      this.activatedRoute.data as Observable<{
+        company: IDataEnterpriseModel;
+        entries: IEntryModel[];
+      }>
+    ).subscribe(({ company, entries }) => {
       this.companyConfiguration.entryOptions = entries;
       this.companyConfiguration.companyData = company;
       this.isInReview =
@@ -58,6 +67,7 @@ export class CompanyConfigurationPage implements OnInit {
   onSendForm() {
     this.companyConfiguration.savePassword().subscribe(() => {
       this.showSidebar = false;
+      this.blurContent = true;
     });
   }
   onShowPanel() {
