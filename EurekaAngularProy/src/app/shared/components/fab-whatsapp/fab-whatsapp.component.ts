8b37d1184d55da9cd64d80443c8e1069d64866bb
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-11-10 12:00:48 -0500

    DDCS-2507 fix(shared): whatsapp button expanded version

--- EurekaAngularProy/src/app/shared/components/fab-whatsapp/fab-whatsapp.component.ts
+++ EurekaAngularProy/src/app/shared/components/fab-whatsapp/fab-whatsapp.component.ts
@@ -14,6 +14,7 @@ import {
 })
 export class FabWhatsappComponent implements OnChanges {
   @Input() showButton = true;
+  @Input() expand = true;
   showText = true;
   url = '';
   timeToHide = 10000;
