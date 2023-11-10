a9e3b7743e28144185e81c47882d7f9199abcd2d
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-11-10 11:59:21 -0500

    DDCS-2507 fix(shared): updated layout footer

--- EurekaAngularProy/src/app/shared/components/footer/footer.component.ts
+++ EurekaAngularProy/src/app/shared/components/footer/footer.component.ts
@@ -1,17 +1,47 @@
 import { Component } from '@angular/core';
+import { DialogService } from 'primeng/dynamicdialog';
 
 import {
   AdobeAnalyticsService,
   AdobeEvent,
 } from '../../services/adobe-analytics.service';
+import { ModalTermsComponent } from '../modal-terms/modal-terms.component';
 
 @Component({
   selector: 'cs-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  providers: [DialogService],
 })
 export class FooterComponent {
-  constructor(private adobeAnalytics: AdobeAnalyticsService) {}
+  constructor(
+    public dialogService: DialogService,
+    private adobeAnalytics: AdobeAnalyticsService
+  ) {}
+
+  showModalTerms() {
+    this.dialogService.open(ModalTermsComponent, {
+      width: '810px',
+      header: 'Términos y condiciones',
+      styleClass: 'modal-custom-cs',
+    });
+
+    this.adobeAnalytics.trackEvent(AdobeEvent.trackAction, {
+      category: 'Términos y condiciones',
+      action: 'Click',
+      detail: 'Términos y condiciones',
+      label: 'Términos y condiciones',
+      typeElement: 'Link',
+      location: 'Footer',
+    });
+
+    this.adobeAnalytics.trackEvent(AdobeEvent.trackView, {
+      category: 'Términos y condiciones',
+      action: 'modal-view',
+      detail: 'Términos y condiciones',
+      location: 'Modal',
+    });
+  }
 
   clickWa() {
     this.adobeAnalytics.trackEvent(AdobeEvent.trackAction, {
