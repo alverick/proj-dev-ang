f4834246ebc13320bb98023ecfe510d5bbb714a9
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-11-13 11:39:56 -0500

    DDCS-2507 chore(storybook): fixed compodoc configuration

--- EurekaAngularProy/.storybook/preview.js
+++ EurekaAngularProy/.storybook/preview.js
@@ -1,5 +1,9 @@
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
+
 export const decorators = [];
 
 export const parameters = {
   layout: 'centered',
 };
+setCompodocJson(docJson);
