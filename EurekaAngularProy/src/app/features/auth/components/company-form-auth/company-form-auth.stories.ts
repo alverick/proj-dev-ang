79ef462d0dce7d9790f2a3194a9828253024352c
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-11-22 15:20:27 -0500

    DDCS-2964 feat(auth): update stories to csf3

--- EurekaAngularProy/src/app/features/auth/components/company-form-auth/company-form-auth.stories.ts
+++ EurekaAngularProy/src/app/features/auth/components/company-form-auth/company-form-auth.stories.ts
@@ -1,28 +1,166 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { moduleMetadata } from '@storybook/angular';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  applicationConfig,
+  Meta,
+  moduleMetadata,
+  StoryObj,
+} from '@storybook/angular';
+import { isNilOrEmpty, isNotNilOrEmpty } from 'ramda-adjunct';
 
+import { errorRegisterAuth } from '../../../../shared/constants/company-errors';
+import { IEntryModel } from '../../../../shared/models';
+import { IDataEnterpriseModel } from '../../../../shared/models/data-enterprise.model';
+import { IErrorMessages } from '../../../../shared/models/forms';
 import { SharedModule } from '../../../../shared/shared.module';
+import { AffiliationFormsService } from '../../services';
 import { CompanyFormAuthComponent } from './company-form-auth.component';
 
-export default {
-  title: 'Auth/Module/Company Form Fill',
+@Component({
+  selector: 'cs-form-demo',
+  template: ` <cs-company-form-auth
+    class="tw-max-w-2xl tw-pl-20"
+    [categories]="entryOptions"
+    [companyForm]="form"
+    [errorMessages]="errorMessages"
+    (sendForm)="onSubmit($event)"
+  ></cs-company-form-auth>`,
+})
+class FormDemoComponent implements OnChanges {
+  @Output() sendForm = new EventEmitter<IDataEnterpriseModel>();
+  form: FormGroup;
+  @Input() errorMessages: IErrorMessages;
+  @Input() companyName = '';
+  entryOptions: IEntryModel[] = [
+    {
+      code: '33',
+      name: 'CLUBS II',
+    },
+    {
+      code: '34',
+      name: 'COLEGIOS II',
+    },
+    {
+      code: '36',
+      name: 'ESTADO II',
+    },
+    {
+      code: '39',
+      name: 'IB OPER.INTII',
+    },
+    {
+      code: '40',
+      name: 'INMOBILIAR II',
+    },
+    {
+      code: '38',
+      name: 'PREPA/RECARII',
+    },
+    {
+      code: '32',
+      name: 'SEGURO/OTROII',
+    },
+    {
+      code: '31',
+      name: 'SERVICIOS II',
+    },
+    {
+      code: '35',
+      name: 'UNIV/INST II',
+    },
+    {
+      code: '37',
+      name: 'VARIOS II',
+    },
+  ];
+  constructor(affiliationForms: AffiliationFormsService) {
+    this.form = affiliationForms.authForm;
+    this.form.patchValue(
+      {
+        name: this.companyName,
+        // documentType: 'DNI',
+        // documentNumber: '93883333',
+        ruc: '20413425183',
+        // email: 'sdfs@fsf.com',
+        // emailConfirm: 'sdfs@fsf.com',
+        // movilNumber: '982222222',
+        // movilOperator: 'M',
+      },
+      { emitEvent: false }
+    );
+    console.log('companyName', this.companyName);
+    if (isNotNilOrEmpty(this.companyName)) {
+      this.form.get('name').disable({ emitEvent: false });
+    }
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    if (changes.companyName) {
+      this.form.patchValue(
+        {
+          name: this.companyName,
+        },
+        { emitEvent: false }
+      );
+      if (isNotNilOrEmpty(this.companyName)) {
+        this.form.get('name').disable({ emitEvent: false });
+      }
+    }
+  }
+  onSubmit($event) {
+    this.sendForm.emit($event);
+  }
+}
+
+const meta: Meta<FormDemoComponent> = {
+  title: 'Auth/Module/Company Form Auth',
+  component: FormDemoComponent,
   decorators: [
+    applicationConfig({ providers: [provideAnimations()] }),
     moduleMetadata({
-      declarations: [CompanyFormAuthComponent],
-      imports: [
-        BrowserAnimationsModule,
-        HttpClientModule,
-        CommonModule,
-        SharedModule,
-      ],
+      declarations: [FormDemoComponent, CompanyFormAuthComponent],
+      imports: [SharedModule],
+      providers: [AffiliationFormsService],
     }),
   ],
 };
 
-export const normal = () => ({
-  component: CompanyFormAuthComponent,
-  template: `<cs-validation-defaults class="tw-hidden"></cs-validation-defaults><cs-company-form-auth></cs-company-form-auth>`,
-  argTypes: { sendForm: { action: 'clicked' } },
-});
+export default meta;
+
+// export default {
+//   title: 'Auth/Module/Company Form Auth',
+//   decorators: [
+//     moduleMetadata({
+//       declarations: [CompanyFormAuthComponent],
+//       imports: [
+//         BrowserAnimationsModule,
+//         HttpClientModule,
+//         CommonModule,
+//         SharedModule,
+//       ],
+//     }),
+//   ],
+// };
+
+type Story = StoryObj<FormDemoComponent>;
+
+export const normal: Story = {
+  args: {
+    errorMessages: errorRegisterAuth,
+  },
+};
+
+export const Named: Story = {
+  args: {
+    errorMessages: errorRegisterAuth,
+    companyName: 'Nombre de compañia',
+  },
+};
