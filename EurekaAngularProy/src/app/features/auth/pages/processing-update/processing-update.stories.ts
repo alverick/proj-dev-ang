78741403fec2d57cf86333c37396fef4688c1a12
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-12-04 10:37:32 -0500

    DDCS-2964 fix(storybook): added stories

---
+++ EurekaAngularProy/src/app/features/auth/pages/processing-update/processing-update.stories.ts
@@ -0,0 +1,29 @@
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+
+import { AffiliationService } from '../../services';
+import { ProcessingUpdatePage } from './processing-update.page';
+
+class MockAffiliationService {
+  email: 'mailmock@mail.com';
+}
+
+const meta: Meta<ProcessingUpdatePage> = {
+  title: 'Page/Update Finished',
+  component: ProcessingUpdatePage,
+  decorators: [
+    moduleMetadata({
+      providers: [
+        { provide: AffiliationService, useClass: MockAffiliationService },
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<ProcessingUpdatePage>;
+
+export const Default: Story = {
+  args: {
+    email: 'mail-item@mail.com',
+  },
+};
---
+++ EurekaAngularProy/src/app/features/auth/pages/registration-finished/registration-finished.stories.ts
@@ -0,0 +1,29 @@
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+
+import { AffiliationService } from '../../services';
+import { RegistrationFinishedPage } from './registration-finished.page';
+
+class MockAffiliationService {
+  email: 'mailmock@mail.com';
+}
+
+const meta: Meta<RegistrationFinishedPage> = {
+  title: 'Page/Registration Finished',
+  component: RegistrationFinishedPage,
+  decorators: [
+    moduleMetadata({
+      providers: [
+        { provide: AffiliationService, useClass: MockAffiliationService },
+      ],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<RegistrationFinishedPage>;
+
+export const Default: Story = {
+  args: {
+    email: 'mail@mail.com',
+  },
+};
