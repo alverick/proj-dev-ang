a1600184c0f8507dce70a00c326f1f52bfcf27d3
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-12-13 10:38:00 -0500

    DDCS-3096 fix(general): eslint fixes

--- EurekaAngularProy/src/app/features/admin/admin-routing.module.ts
+++ EurekaAngularProy/src/app/features/admin/admin-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { appRoutingNames } from 'src/app/app-routing.collection';
+
 import { AuthGuard } from '../../shared/guards/auth.guard';
 import { GtpInputGuard } from '../../shared/guards/gtp-input.guard';
+import { AdminComponent } from './admin.component';
 import {
   adminDynamicRoutingNames,
   adminRoutingNames,
 } from './admin-routing.names';
-import { AdminComponent } from './admin.component';
 import { AprobacionesPage } from './pages/aprobaciones/aprobaciones.page';
 import { CargaHistoricoComponent } from './pages/carga-historico/carga-historico.component';
 import { ConfigurarCorreoGtpComponent } from './pages/configurar-correo-gtp/configurar-correo-gtp.component';
