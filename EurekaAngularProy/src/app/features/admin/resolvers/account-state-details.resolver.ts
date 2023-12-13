9d87d39ae8560c92ad27545021189522d6a46969
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-12-13 10:38:00 -0500

    DDCS-3096 fix(general): eslint fixes

--- EurekaAngularProy/src/app/features/admin/resolvers/account-state-details.resolver.ts
+++ EurekaAngularProy/src/app/features/admin/resolvers/account-state-details.resolver.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
-import { of, Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { IAccountStateDetails } from 'src/app/shared/models/company';
+
 import { CompanyService } from '../../../shared/services';
 
 @Injectable()
@@ -11,10 +12,12 @@ export class AccountStateDetailsResolver implements Resolve<any> {
   resolve(
     route: ActivatedRouteSnapshot
   ): Observable<IAccountStateDetails | string> {
-    return this.companyService.getAccountStateDetails(route.params.llave).pipe(
-      catchError(() => {
-        return of('No data');
-      })
-    );
+    return this.companyService
+      .getAccountStateDetails(route.params.llave as string)
+      .pipe(
+        catchError(() => {
+          return of('No data');
+        })
+      );
   }
 }
