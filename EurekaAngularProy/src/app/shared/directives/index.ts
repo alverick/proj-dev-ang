92750fba59994bc70253ff7fef8c129f9f06bbdd
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2023-12-27 12:35:12 -0500

    DDCS-3096 refactor(shared): moved directive

--- EurekaAngularProy/src/app/shared/directives/index.ts
+++ EurekaAngularProy/src/app/shared/directives/index.ts
@@ -1,13 +1,16 @@
+import { AmountMaxValidateDirective } from './amount-max-validate.directive';
 import { InputMoneyDirective } from './input-money.directive';
 import { InputTrimSpacesDirective } from './input-trim-spaces.directive';
 import { InputWithoutSpacesDirective } from './input-without-spaces.directive';
 
 export const DIRECTIVES = [
+  AmountMaxValidateDirective,
   InputMoneyDirective,
   InputWithoutSpacesDirective,
   InputTrimSpacesDirective,
 ];
 
+export { AmountMaxValidateDirective } from './amount-max-validate.directive';
 export { InputMoneyDirective } from './input-money.directive';
 export { InputTrimSpacesDirective } from './input-trim-spaces.directive';
 export { InputWithoutSpacesDirective } from './input-without-spaces.directive';
