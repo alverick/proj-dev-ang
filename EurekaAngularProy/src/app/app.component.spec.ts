d7c1458ed03ac86c4e356d95c01a5841fddd1396
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-01-04 11:30:48 -0500

    DDCS-2959 test(app): update test app

--- EurekaAngularProy/src/app/app.component.spec.ts
+++ EurekaAngularProy/src/app/app.component.spec.ts
@@ -1,31 +1,41 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ValdemortModule } from 'ngx-valdemort';
 
 import { AppComponent } from './app.component';
+import { FabWhatsappComponent } from './shared/components/fab-whatsapp/fab-whatsapp.component';
+import { ValidationDefaultsComponent } from './shared/components/validation-defaults/validation-defaults.component';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
+    void TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        ValidationDefaultsComponent,
+        FabWhatsappComponent,
+      ],
+      imports: [RouterTestingModule, ValdemortModule, NgxSpinnerModule],
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.debugElement.componentInstance as AppComponent;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'Mis Cobros – Interbank'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Mis Cobros – Interbank');
+    const app = fixture.debugElement.componentInstance as AppComponent;
+    expect(app.title).toEqual('Cobro Simple – Interbank');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
       'Welcome to Mis Cobros – Interbank!'
     );
   });
