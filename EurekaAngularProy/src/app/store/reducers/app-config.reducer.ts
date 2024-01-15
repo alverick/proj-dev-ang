15c5111d38d4e21bd968b456aacccdf07589fe4f
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-01-15 17:27:54 -0500

    DDCOBSIM-2 feat(store): added app config module

---
+++ EurekaAngularProy/src/app/store/reducers/app-config.reducer.ts
@@ -0,0 +1,27 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+
+import * as AppConfigActions from '../actions/app-config.actions';
+
+export const appConfigFeatureKey = 'appConfig';
+
+export interface State {
+  loaded: boolean;
+  disabledAffiliation: boolean;
+}
+
+export const initialState: State = {
+  loaded: false,
+  disabledAffiliation: true,
+};
+
+export const reducer = createReducer(
+  initialState,
+  on(AppConfigActions.configAppConfigs, (state) => state),
+  on(AppConfigActions.configAppConfigsSuccess, (state, action) => state),
+  on(AppConfigActions.configAppConfigsFailure, (state, action) => state)
+);
+
+export const appConfigFeature = createFeature({
+  name: appConfigFeatureKey,
+  reducer,
+});
