fc6d14241a8a0cc0bd7b1a6421c52be979ceaabf
Jorge Suarez <lecqrvc2bseiwstcch7ge3pe3emjsy@bots.bitbucket.org>
2024-02-02 13:44:15 -0500

    DDCOBSIM-19 refactor(general): removed useless function typings

--- EurekaAngularProy/src/app/features/auth/services/affiliation.service.ts
+++ EurekaAngularProy/src/app/features/auth/services/affiliation.service.ts
@@ -675,7 +675,7 @@ export class AffiliationService {
     };
   }
 
-  public getEntryOptions(): Observable<IEntryModel[]> {
+  public getEntryOptions() {
     if (isNotNilOrEmpty(this.entryOptions)) {
       return of(this.entryOptions);
     }
@@ -738,7 +738,7 @@ Te llevaremos a abrir una Cuenta Negocios 100% digital.`,
     });
   }
 
-  public getAccountsCompany(): Observable<any[]> {
+  public getAccountsCompany() {
     if (this.companyId) {
       return this.companyService.getCompanyAccountsById(this.companyId);
     }
