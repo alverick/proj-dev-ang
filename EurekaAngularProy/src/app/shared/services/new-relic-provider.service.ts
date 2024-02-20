5b05a9f0eb5894635363ffff86717971a6678225
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-02-20 08:25:07 -0500

    DDCOBSIM-6 fix(shared): flat metadata for new relic

--- EurekaAngularProy/src/app/shared/services/new-relic-provider.service.ts
+++ EurekaAngularProy/src/app/shared/services/new-relic-provider.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { type BrowserAgent } from '@newrelic/browser-agent/loaders/browser-agent';
 import dot from 'dot-object';
+import { pathOr } from 'ramda';
 import { filter } from 'rxjs/operators';
 
 import { type ProviderService } from './provider.service';
 import {
   type AdobeEventType,
+  type Metadata,
   type TrackEventProperties,
   AdobeEvent,
   TrackingService,
@@ -29,10 +31,15 @@ export class NewRelicProviderService implements ProviderService {
   }
 
   trackEvent(event: AdobeEventType, payload: Partial<TrackEventProperties>) {
-    window.newrelic.addPageAction(
-      event,
-      dot.dot(payload) as Record<string, string>
+    const metadata = pathOr<Metadata[]>([], ['action', 'metadata'], payload);
+    const data = Object.fromEntries(
+      metadata.map((meta) => [meta.key, meta.value])
     );
+    window.newrelic.addPageAction(event, {
+      ...data,
+      state: payload.action.state,
+      typeError: payload.action.typeError,
+    });
   }
 
   startTracking(): void {
@@ -41,13 +48,15 @@ export class NewRelicProviderService implements ProviderService {
         filter(
           ({ event, payload }) =>
             event === AdobeEvent.trackFormSubmit &&
+            (payload.action.step === 'Step2' ||
+              payload.action.step === 'Step5') &&
             payload.page.module === 'Afiliación'
         )
       )
       .subscribe(({ event, payload }) => {
-        console.log('trackingService.eventSubject$', event);
         this.trackEvent(event, payload);
       });
+
     this.trackingService.pageSubject$
       .pipe(filter(() => this.enabledPageRouting))
       .subscribe((payload) => this.trackPage(payload));
