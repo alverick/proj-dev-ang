d61bd80687ee65b93b6f35e245ebe866184b1616
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-02-23 08:38:34 -0500

    DDCOBSIM-6 fix(shared): export messages

--- EurekaAngularProy/src/app/shared/constants/analytics-messages.ts
+++ EurekaAngularProy/src/app/shared/constants/analytics-messages.ts
@@ -1,3 +1,4 @@
 export const typeErrorServer = 'Ha ocurrido un error con el servidor';
 export const stateIntent = 'Intención de envío';
 export const stateSuccessful = 'Envío exitoso';
+export const notAvailable = 'Not available';
