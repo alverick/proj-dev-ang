f73d6cebd53d5bd17bd5695a69702159724fa53e
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-02-23 08:41:41 -0500

    DDCOBSIM-6 fix(shared): added tests

--- EurekaAngularProy/src/app/shared/services/new-relic-provider.service.spec.ts
+++ EurekaAngularProy/src/app/shared/services/new-relic-provider.service.spec.ts
@@ -1,16 +1,83 @@
 import { TestBed } from '@angular/core/testing';
+import type { BrowserAgent } from '@newrelic/browser-agent/loaders/browser-agent';
+import dot from 'dot-object';
 
 import { NewRelicProviderService } from './new-relic-provider.service';
+import { AdobeEvent } from './tracking.service';
+
+declare const window: {
+  newrelic: Partial<BrowserAgent>;
+} & Window;
 
 describe('NewRelicProviderService', () => {
   let service: NewRelicProviderService;
+  const newrelic = {
+    addPageAction: () => {
+      console.log('addPageAction');
+    },
+  };
+  window.newrelic = newrelic;
+  const mockedTrackPage = jest.fn();
+  const newRelicActionOrig = window.newrelic.addPageAction;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [NewRelicProviderService],
+    });
     service = TestBed.inject(NewRelicProviderService);
+    window.newrelic.addPageAction = mockedTrackPage;
+  });
+
+  afterEach(() => {
+    window.newrelic.addPageAction = newRelicActionOrig;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('track page', () => {
+    const payload = {
+      user: {
+        codEmpresa: '',
+        codGrupo: '',
+        codRuc: '',
+        digitalId: '',
+        userId: '',
+      },
+    };
+    service.trackPage(payload);
+    expect(mockedTrackPage).toBeCalled();
+    expect(mockedTrackPage).toHaveBeenCalledWith(
+      AdobeEvent.pageTrack,
+      dot.dot(payload)
+    );
+  });
+
+  it('track event', () => {
+    const payload = {
+      user: {
+        codEmpresa: '',
+        codGrupo: '',
+        codRuc: '',
+        digitalId: '',
+        userId: '',
+      },
+      action: {
+        state: 'state test',
+        metadata: [
+          {
+            key: 'key',
+            value: 'value',
+          },
+        ],
+      },
+    };
+    service.trackEvent(AdobeEvent.trackFormSubmit, payload);
+    expect(mockedTrackPage).toBeCalled();
+    expect(mockedTrackPage).toHaveBeenCalledWith(AdobeEvent.trackFormSubmit, {
+      key: 'value',
+      state: 'state test',
+    });
   });
 });
--- EurekaAngularProy/src/app/shared/services/tracking.service.spec.ts
+++ EurekaAngularProy/src/app/shared/services/tracking.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TrackingService } from './tracking.service';
+import { AdobeEvent, TrackingService } from './tracking.service';
 
 describe('TrackingService', () => {
   let service: TrackingService;
@@ -12,5 +12,41 @@ describe('TrackingService', () => {
 
   it('should be created', () => {
     void expect(service).toBeTruthy();
+  });
+
+  it('set ruc', () => {
+    const ruc = '10293726253';
+    service.setRuc(ruc);
+    void expect(service.payload.user.codRuc).toEqual(ruc);
+  });
+
+  it('track event view', (done) => {
+    const eventProperties = { category: 'category' };
+    service.eventSubject$.subscribe(({ event, payload }) => {
+      void expect(event).toEqual(AdobeEvent.trackView);
+      void expect(payload.view).toEqual(eventProperties);
+      done();
+    });
+    service.trackEvent(AdobeEvent.trackView, eventProperties);
+  });
+
+  it('track event action', (done) => {
+    const eventProperties = { category: 'category' };
+    service.eventSubject$.subscribe(({ event, payload }) => {
+      void expect(event).toEqual(AdobeEvent.trackAction);
+      void expect(payload.action).toEqual(eventProperties);
+      done();
+    });
+    service.trackEvent(AdobeEvent.trackAction, eventProperties);
+  });
+
+  it('track page', (done) => {
+    service.pageSubject$.subscribe((payload) => {
+      expect(payload.page).toEqual(
+        expect.objectContaining({ name: 'cs:login' })
+      );
+      done();
+    });
+    service.trackPage('/login');
   });
 });
