0f9eb22740220ed5b7fde8c99eb9432c693f14c6
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-02-23 08:41:41 -0500

    DDCOBSIM-6 fix(shared): added tests

--- EurekaAngularProy/src/app/shared/services/adobe-launch-provider.service.spec.ts
+++ EurekaAngularProy/src/app/shared/services/adobe-launch-provider.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AdobeLaunchProviderService } from './adobe-launch-provider.service';
+import {
+  type Satellite,
+  AdobeLaunchProviderService,
+} from './adobe-launch-provider.service';
 
 describe('AdobeLaunchProviderService', () => {
   let service: AdobeLaunchProviderService;
+  const _satellite: Partial<Satellite> = {
+    track: () => {
+      console.log('addPageAction');
+    },
+  };
+
+  const newRelicActionOrig = _satellite.track;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [AdobeLaunchProviderService] });
     service = TestBed.inject(AdobeLaunchProviderService);
+  });
+
+  afterEach(() => {
+    _satellite.track = newRelicActionOrig;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('track page', () => {
+    const payload = {
+      user: {
+        codEmpresa: '',
+        codGrupo: '',
+        codRuc: '',
+        digitalId: '',
+        userId: '',
+      },
+    };
+    service.trackPage(payload);
+    // expect(mockedTrackPage).toBeCalled();
   });
 });
