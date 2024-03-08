88f6dba2f853b48b4699735ad84ff66af728447d
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-03-08 12:23:59 -0500

    DDCOBSIM-184 test(store): update tests

--- EurekaAngularProy/src/app/store/collections/collect-amount.service.spec.ts
+++ EurekaAngularProy/src/app/store/collections/collect-amount.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { MockProvider } from 'ng-mocks';
 
 import { CollectAmountService } from './collect-amount.service';
 
@@ -6,7 +7,9 @@ describe('CollectAmountService', () => {
   let service: CollectAmountService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [MockProvider(CollectAmountService)],
+    });
     service = TestBed.inject(CollectAmountService);
   });
 
