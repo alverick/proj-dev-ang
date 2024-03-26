3c5e8e01cf736dee2e2f929b312f58a8bdd07ef0
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-03-26 10:57:36 -0500

    DDCOBSIM-184 test(general): fix dependencies test

--- EurekaAngularProy/src/app/shared/components/service-edit-form/service-edit-form.component.spec.ts
+++ EurekaAngularProy/src/app/shared/components/service-edit-form/service-edit-form.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { type ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MockProvider } from 'ng-mocks';
 import { LoggerModule } from 'ngx-logger';
 import { CheckboxModule } from 'primeng/checkbox';
 import { DropdownModule } from 'primeng/dropdown';
@@ -10,7 +11,11 @@ import { MessagesModule } from 'primeng/messages';
 import { environment } from '../../../../environments/environment';
 import { CompanyServicesService } from '../../../features/internal/services';
 import { errorServiceConfiguration } from '../../constants/company-errors';
-import { CompanyService, ServicesFormsService } from '../../services';
+import {
+  CompanyService,
+  DigitalDataService,
+  ServicesFormsService,
+} from '../../services';
 import { LabelControlComponent } from '../label-control/label-control.component';
 import { ServiceEditFormComponent } from './service-edit-form.component';
 
@@ -37,7 +42,12 @@ describe('ServiceEditFormComponent', () => {
         HttpClientTestingModule,
         ReactiveFormsModule,
       ],
-      providers: [CompanyService, CompanyServicesService, ServicesFormsService],
+      providers: [
+        CompanyService,
+        CompanyServicesService,
+        MockProvider(DigitalDataService),
+        ServicesFormsService,
+      ],
     }).compileComponents();
   });
 
