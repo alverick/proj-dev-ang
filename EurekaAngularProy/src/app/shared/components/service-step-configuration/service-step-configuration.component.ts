8cdaa4f40da6c54bb03e8c21c44058538673b818
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-03-26 10:58:13 -0500

    DDCOBSIM-184 refactor(shared): eslint fixes

--- EurekaAngularProy/src/app/shared/components/service-step-configuration/service-step-configuration.component.ts
+++ EurekaAngularProy/src/app/shared/components/service-step-configuration/service-step-configuration.component.ts
@@ -1,15 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  type OnInit,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
-import { ISelectOptions } from '../../constants/company';
+import { type ISelectOptions } from '../../constants/company';
 import { debtorCodeCustomEmpty } from '../../constants/services';
 import { IErrorMessages, ModelFormGroup } from '../../models/forms';
 import {
-  ServiceConfigurationForm,
-  ServiceDebt,
+  type ServiceConfigurationForm,
+  type ServiceDebt,
+  type ServiceTypeType,
   ServiceTypes,
-  ServiceTypeType,
 } from '../../services/services-forms.service';
 
 @Component({
