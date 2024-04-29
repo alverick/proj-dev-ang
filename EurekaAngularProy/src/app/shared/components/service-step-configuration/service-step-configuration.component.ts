2eadd343a78c3546fd3e8b39989a795f2dcf6745
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-04-29 10:26:13 -0500

    DDCOBSIM-683 fix(general): changing variable isNewFlow to collection restriction

--- EurekaAngularProy/src/app/shared/components/service-step-configuration/service-step-configuration.component.ts
+++ EurekaAngularProy/src/app/shared/components/service-step-configuration/service-step-configuration.component.ts
@@ -58,7 +58,7 @@ export class ServiceStepConfigurationComponent implements OnInit {
   @Input() chargeTypeOptions: ISelectOptions[];
   @Input() interestTypeOptions: ISelectOptions[];
   @Input() showCancel = false;
-  @Input() showAllTypes = false;
+  @Input() showAllTypes = true;
   debtForm: ModelFormGroup<ServiceDebt>;
   showDebtFields = false;
   debtorCodeEditable = false;
--- EurekaAngularProy/src/app/shared/services/services-forms.service.ts
+++ EurekaAngularProy/src/app/shared/services/services-forms.service.ts
@@ -48,7 +48,7 @@ export class ServicesFormsService {
   serviceForm: ModelFormGroup<ServiceFormValue>;
   serviceConfigForm: ModelFormGroup<ServiceConfigurationForm>;
   editServiceForm: ModelFormGroup<ServiceEditForm>;
-  defaultServiceType: ServiceTypeType = ServiceTypes.complete;
+  defaultServiceType: ServiceTypeType = ServiceTypes.withoutData;
 
   editNameValidators = [
     Validators.required,
