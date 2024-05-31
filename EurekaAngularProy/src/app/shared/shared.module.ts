4cb16c16b75d2bfbcad663d966e3d9ec2e7b60f0
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-05-31 14:54:59 -0500

    DDCOBSIM-311 refactor(general): updated directive

--- EurekaAngularProy/src/app/shared/shared.module.ts
+++ EurekaAngularProy/src/app/shared/shared.module.ts
@@ -81,7 +81,7 @@ import { DATA_SERVICES } from './data';
 import { DIRECTIVES } from './directives';
 import { NameEnterpiseDirective } from './directives/name-enterpise.directive';
 import { OnlyNumbersFormDirective } from './directives/only-numbers-form.directive';
-import { OnlynumbersDirective } from './directives/onlynumbers.directive';
+import { OnlyNumbersDirective } from './directives/only-numbers.directive';
 import { SearchDirective } from './directives/search.directive';
 import { PIPES } from './pipes';
 import { RESOLVERS } from './resolvers';
@@ -168,7 +168,7 @@ const UI_MODULES = [
   ],
   declarations: [
     OnlyNumbersFormDirective,
-    OnlynumbersDirective,
+    OnlyNumbersDirective,
     SearchDirective,
     NameEnterpiseDirective,
     HeaderComponent,
@@ -192,7 +192,7 @@ const UI_MODULES = [
   ],
   exports: [
     OnlyNumbersFormDirective,
-    OnlynumbersDirective,
+    OnlyNumbersDirective,
     SearchDirective,
     NameEnterpiseDirective,
     ...DIRECTIVES,
