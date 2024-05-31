fc131d26ff7f23f1a4ceb66b43ca76b890b87fbe
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-05-31 10:53:58 -0500

    DDCOBSIM-311 refactor(general): eslint fixes

--- EurekaAngularProy/src/app/store/dataservices/historical-collect-data.service.ts
+++ EurekaAngularProy/src/app/store/dataservices/historical-collect-data.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { DefaultDataService, HttpUrlGenerator, QueryParams } from '@ngrx/data';
+import {
+  type QueryParams,
+  DefaultDataService,
+  HttpUrlGenerator,
+} from '@ngrx/data';
 import { Store } from '@ngrx/store';
-import { Observable, of } from 'rxjs';
+import { type Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { DashboardDataService } from '../../shared/data';
-import { HistoricalCollect } from '../entities';
+import { type HistoricalCollect } from '../entities';
 
 @Injectable()
 export class HistoricalCollectDataService extends DefaultDataService<HistoricalCollect> {
