95d2b733f43ab1c0d1b0ec575cf4324fe7baa510
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-06-04 17:21:23 -0500

    DDCOBSIM-304 refactor(app): replaced after content checked

--- EurekaAngularProy/src/app/app.component.ts
+++ EurekaAngularProy/src/app/app.component.ts
@@ -1,13 +1,10 @@
-import {
-  type AfterContentChecked,
-  type OnInit,
-  Component,
-} from '@angular/core';
+import { type OnInit, Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { NavigationEnd, Router } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 import { isNil } from 'ramda';
+import { delay } from 'rxjs';
 
 import { appFullRoutingNames } from './app-routing.names';
 import { primeng } from './shared/lang/es';
@@ -22,7 +19,7 @@ import { LoginService } from './shared/services/login.service';
   templateUrl: './app.component.html',
   providers: [LoginService],
 })
-export class AppComponent implements OnInit, AfterContentChecked {
+export class AppComponent implements OnInit {
   title = 'Cobro Simple – Interbank';
   showButton = false;
   expand = false;
@@ -43,6 +40,8 @@ export class AppComponent implements OnInit, AfterContentChecked {
         this.showButton = !val.url.startsWith(appFullRoutingNames.ADMIN);
       }
     });
+
+    this.detectLayoutForm();
 
     matIconRegistry.addSvgIcon(
       'eurc_calendar',
@@ -77,11 +76,16 @@ export class AppComponent implements OnInit, AfterContentChecked {
     );
   }
 
+  detectLayoutForm() {
+    this.router.events.pipe(delay(300)).subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.expand = isNil(document.querySelector('cs-layout-form'));
+      }
+    });
+  }
+
   ngOnInit() {
     this.primengConfig.ripple = true;
     this.primengConfig.setTranslation(primeng);
-  }
-  ngAfterContentChecked() {
-    this.expand = isNil(document.querySelector('cs-layout-form'));
   }
 }
