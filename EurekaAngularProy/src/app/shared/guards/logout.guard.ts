2a1d57be6146522a101de19ef0944b7ce5ad5a70
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-06-04 17:22:28 -0500

    DDCOBSIM-304 refactor(shared): removes unused code

--- EurekaAngularProy/src/app/shared/guards/logout.guard.ts
+++ EurekaAngularProy/src/app/shared/guards/logout.guard.ts
@@ -1,19 +1,13 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { type CanActivate, Router } from '@angular/router';
 
 import { internalFullRoutingNames } from '../../features/internal/internal-routing.names';
-import { StorageService } from '../services/storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LogoutGuard implements CanActivate {
-  constructor(
-    private router: Router,
-    private storageService: StorageService,
-    private spinner: NgxSpinnerService
-  ) {}
+  constructor(private router: Router) {}
   canActivate(): boolean {
     if (
       sessionStorage.getItem('tk') == null ||
@@ -25,7 +19,7 @@ export class LogoutGuard implements CanActivate {
     // si el token existe
     if (sessionStorage.getItem('tk')) {
       // entonces enviame al home
-      this.router.navigate([internalFullRoutingNames.HOME]);
+      void this.router.navigate([internalFullRoutingNames.HOME]);
       // que no me deje ver el login o
       return false;
     }
