153c56311fc2d84a51c8f47ea608ad5d9119c3eb
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-06-17 12:59:20 -0500

    DDCOBSIM-307 fix(shared): header as standalone component

--- EurekaAngularProy/src/app/shared/components/header/header.component.ts
+++ EurekaAngularProy/src/app/shared/components/header/header.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'cs-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, NgOptimizedImage],
 })
 export class HeaderComponent {
   @Input() isBlank = false;
