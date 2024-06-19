e3f69537899d96e244500dc4a8d8eaa3a0fe00cf
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-06-19 08:14:31 -0500

    DDCOBSIM-304 refactor(app): moved transitions

---
+++ EurekaAngularProy/src/app/shared/animations/page-transitions.ts
@@ -0,0 +1,51 @@
+import {
+  animate,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+export const fadeAnimation = trigger('fadeAnimation', [
+  transition('* => *', [
+    query(
+      ':enter',
+      [
+        style({
+          opacity: 0,
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+        }),
+      ],
+      { optional: true }
+    ),
+    query(
+      ':leave',
+      // here we apply a style and use the animate function to apply the style over 0.3 seconds
+      [
+        style({
+          opacity: 1,
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+        }),
+        animate('0.7s', style({ opacity: 0 })),
+      ],
+      { optional: true }
+    ),
+    query(
+      ':enter',
+      [
+        style({
+          opacity: 0,
+          position: 'relative',
+          height: '100%',
+          width: '100%',
+        }),
+        animate('0.7s', style({ opacity: 1 })),
+      ],
+      { optional: true }
+    ),
+  ]),
+]);
