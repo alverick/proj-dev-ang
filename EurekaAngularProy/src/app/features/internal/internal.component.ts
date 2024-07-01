9be5668e051f4a06b84e4fd33b8e710bce063405
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-07-01 10:12:26 -0500

    DDCOBSIM-304 fix(internal): add transitions to internal module

--- EurekaAngularProy/src/app/features/internal/internal.component.ts
+++ EurekaAngularProy/src/app/features/internal/internal.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { fadeAnimation } from '../../shared/animations/page-transitions';
+
 @Component({
   selector: 'cs-internal',
   templateUrl: './internal.component.html',
   styleUrls: ['./internal.component.scss'],
+  animations: [fadeAnimation],
 })
 export class InternalComponent {}
