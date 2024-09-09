92bb9f2f88e13ed96460d58a1134c8524d4dc912
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-09-09 16:12:58 -0500

    DDCOBSIM-648 refactor(internal): create dialog config variable

--- EurekaAngularProy/src/app/features/internal/pages/home/home.page.ts
+++ EurekaAngularProy/src/app/features/internal/pages/home/home.page.ts
@@ -152,6 +152,12 @@ export class HomePage implements OnInit, AfterViewInit, OnDestroy {
   private showedCommissions: boolean;
   private readonly onboardingIntro = 'intro';
 
+  protected dialogConfig = {
+    width: '899px',
+    backdropClass: 'backdrop-background-opaque',
+    panelClass: 'upload-files-dialog',
+  };
+
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.updatePositionModal();
@@ -320,10 +326,7 @@ export class HomePage implements OnInit, AfterViewInit, OnDestroy {
       } else if (m.status === 'rejected') {
         this.fileLoad.close();
         this.excelService.statusUpload = false;
-        const dialogRef = this.dialog.open(DialogComponent, {
-          width: '899px',
-          backdropClass: 'backdrop-background-opaque',
-        });
+        const dialogRef = this.dialog.open(DialogComponent, this.dialogConfig);
         dialogRef.componentInstance.ready = true;
         dialogRef.componentInstance.rowsAccepted = m.rowsAccepted;
         dialogRef.componentInstance.rowsRejected = m.rowsRejected;
@@ -996,12 +999,7 @@ export class HomePage implements OnInit, AfterViewInit, OnDestroy {
     this.cargaExcel = false;
     this.excelService.service = service;
     if (this.fileLoad.isRunning()) {
-      const dialogRef = this.dialog.open(DialogComponent, {
-        width: '899px',
-        backdropClass: 'backdrop-background-opaque',
-        // height: '377px',
-        // disableClose: true
-      });
+      const dialogRef = this.dialog.open(DialogComponent, this.dialogConfig);
       dialogRef.afterClosed().subscribe((result: Observable<any>) => {
         dialogRef.componentInstance.ready = false;
         this.fileLoad.verify(this.fileLoadContainer);
@@ -1057,10 +1055,10 @@ export class HomePage implements OnInit, AfterViewInit, OnDestroy {
                 location: 'Modal',
               });
             } else {
-              const dialogRef = this.dialog.open(DialogComponent, {
-                width: '899px',
-                backdropClass: 'backdrop-background-opaque',
-              });
+              const dialogRef = this.dialog.open(
+                DialogComponent,
+                this.dialogConfig
+              );
               dialogRef.afterClosed().subscribe((result: Observable<any>) => {
                 dialogRef.componentInstance.ready = false;
                 this.fileLoad.verify(this.fileLoadContainer);
