5baddfdea987182be9801784292c567a54948669
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-09-27 11:40:13 -0500

    DDCOBSIM-683 feat: added mocks

---
+++ EurekaAngularProy/src/app/shared/mocks/notification.ts
@@ -0,0 +1,63 @@
+export const messages = [
+  {
+    id: 2040,
+    message: '(2010920) ha pagado $2,000.00 del servicio Cursos verano',
+    create: '2023-12-27T16:21:41.153',
+    isNew: true,
+  },
+  {
+    id: 1755,
+    message:
+      'Adri issue(A1123) ha pagado S/100.00 del servicio Matricula Enfermeria',
+    create: '2023-06-19T21:02:32.937',
+    isNew: false,
+  },
+  {
+    id: 1742,
+    message: '(A12345678) ha pagado $1,325.79 del servicio Cursos verano',
+    create: '2023-06-09T21:16:00.983',
+    isNew: true,
+  },
+  {
+    id: 1741,
+    message:
+      'Julio(A0009) ha pagado S/120.00 del servicio Matricula Enfermeria',
+    create: '2023-06-09T19:41:01.353',
+    isNew: true,
+  },
+  {
+    id: 1734,
+    message:
+      'No se aprobaron los cambios en la informacion de tu empresa que solicitaste',
+    create: '2023-06-07T23:38:43.337',
+    isNew: true,
+  },
+  {
+    id: 1733,
+    message:
+      'No se aprobaron los cambios en la informacion de tu empresa que solicitaste',
+    create: '2023-06-07T23:37:12.46',
+    isNew: true,
+  },
+  {
+    id: 1732,
+    message:
+      'No se aprobaron los cambios en la informacion de tu empresa que solicitaste',
+    create: '2023-06-07T23:32:15.173',
+    isNew: true,
+  },
+  {
+    id: 1719,
+    message:
+      'VERA RODRIGUEZ(10004) ha pagado S/100.00 del servicio Matricula Enfermeria',
+    create: '2023-05-23T20:37:02.353',
+    isNew: true,
+  },
+  {
+    id: 1715,
+    message:
+      'Patty(DCPM0003) ha pagado S/3,030.00 de la cuota pago marzo en el servicio Matricula Informatica',
+    create: '2023-05-23T18:44:30.673',
+    isNew: true,
+  },
+];
