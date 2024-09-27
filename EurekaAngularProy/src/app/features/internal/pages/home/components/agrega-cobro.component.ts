4d469a22cb0dc9828f8215ae75f1e9255f7929a7
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-09-27 12:04:15 -0500

    DDCOBSIM-683 fix: removed material dialog components

--- EurekaAngularProy/src/app/features/internal/pages/home/components/agrega-cobro.component.ts
+++ EurekaAngularProy/src/app/features/internal/pages/home/components/agrega-cobro.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { isNotNilOrEmpty } from 'ramda-adjunct';
 import { filter } from 'rxjs/operators';
 
@@ -17,15 +17,15 @@ export class AgregaCobroComponent {
   useAmountLimits = false;
   constructor(
     public excelService: ExcelService,
-    public dialogRef: MatDialogRef<AgregaCobroComponent>,
-    private store: Store
+    public dialogRef: DynamicDialogRef<AgregaCobroComponent>,
+    private store: Store,
   ) {
     this.store
       .select(companyFeature.selectCurrencyLimits)
       .pipe(filter((data) => isNotNilOrEmpty(data)))
       .subscribe((limits) => {
         this.limitAmountMax = limits.find(
-          (limit) => limit.symbol === this.excelService.service.currencySymbol
+          (limit) => limit.symbol === this.excelService.service.currencySymbol,
         )?.limitMax;
       });
     this.store
