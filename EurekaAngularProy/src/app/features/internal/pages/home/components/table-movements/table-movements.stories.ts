406281df4253b3a15262f652ef9dae8546362396
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-09-27 12:03:21 -0500

    DDCOBSIM-683 fix: updated story removed material components

--- EurekaAngularProy/src/app/features/internal/pages/home/components/table-movements/table-movements.stories.ts
+++ EurekaAngularProy/src/app/features/internal/pages/home/components/table-movements/table-movements.stories.ts
@@ -1,51 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { APP_INITIALIZER } from '@angular/core';
-import {
-  DateAdapter,
-  MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE,
-} from '@angular/material/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import {
-  MAT_MOMENT_DATE_FORMATS,
-  MomentDateAdapter,
-} from '@angular/material-moment-adapter';
-import { DomSanitizer } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { moduleMetadata } from '@storybook/angular';
+import { Store } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
+import { applicationConfig, moduleMetadata } from '@storybook/angular';
 import { LoggerModule } from 'ngx-logger';
 import { isNil } from 'ramda';
 
 import { environment } from '../../../../../../../environments/environment';
 import { movementsDataMock } from '../../../../../../shared/mocks/home';
+import { initialState } from '../../../../../../shared/mocks/store';
+import { TrackingService } from '../../../../../../shared/services';
+import { StorageService } from '../../../../../../shared/services/storage.service';
 import { SharedModule } from '../../../../../../shared/shared.module';
 import { SelectAllTableService } from '../../../../services';
 import { TableMovementsComponent } from './table-movements.component';
-
-function initAppComponentFactory(
-  matIconRegistry: MatIconRegistry,
-  domSanitizer: DomSanitizer
-) {
-  return async () => {
-    matIconRegistry.addSvgIcon(
-      'eurc_calendar',
-      domSanitizer.bypassSecurityTrustResourceUrl(
-        '/assets/images/calendar.svg'
-      ),
-      { viewBox: '0 0 24 24' }
-    );
-  };
-}
 
 export default {
   title: 'Internal/Home/Movements Table',
   component: TableMovementsComponent,
   decorators: [
+    applicationConfig({
+      providers: [importProvidersFrom([BrowserAnimationsModule])],
+    }),
     moduleMetadata({
       declarations: [],
       imports: [
-        BrowserAnimationsModule,
         HttpClientModule,
         CommonModule,
         SharedModule,
@@ -57,21 +38,11 @@ export default {
         }),
       ],
       providers: [
-        {
-          provide: APP_INITIALIZER,
-          useFactory: initAppComponentFactory,
-          multi: true,
-          deps: [MatIconRegistry, DomSanitizer],
-        },
-        { provide: MAT_DATE_LOCALE, useValue: 'es-PE' },
-        {
-          provide: DateAdapter,
-          useClass: MomentDateAdapter,
-          deps: [MAT_DATE_LOCALE],
-        },
-
-        { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
         SelectAllTableService,
+        TrackingService,
+        StorageService,
+        Store,
+        provideMockStore({ initialState }),
       ],
     }),
   ],
