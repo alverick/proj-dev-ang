9cd99db1363579ab05e13f310b94911addb6e24a
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-09-27 11:42:47 -0500

    DDCOBSIM-683 feat: added close all dialogs

--- EurekaAngularProy/src/app/shared/services/dynamic-dialog.service.ts
+++ EurekaAngularProy/src/app/shared/services/dynamic-dialog.service.ts
@@ -1,15 +1,15 @@
 import { DOCUMENT } from '@angular/common';
 import {
-  type Type,
   ApplicationRef,
   Inject,
   Injectable,
   Injector,
+  type Type,
 } from '@angular/core';
 import {
+  DialogService,
   type DynamicDialogConfig,
   type DynamicDialogRef,
-  DialogService,
 } from 'primeng/dynamicdialog';
 import { pathOr } from 'ramda';
 
@@ -17,25 +17,29 @@ import { AdobeEvent, TrackingService } from './tracking.service';
 
 @Injectable()
 export class DynamicDialogService extends DialogService {
+  private readonly stackRefs: DynamicDialogRef[] = [];
   constructor(
-    private tracking: TrackingService,
+    private readonly tracking: TrackingService,
     appRef: ApplicationRef,
     injector: Injector,
-    @Inject(DOCUMENT) document: Document
+    @Inject(DOCUMENT) document: Document,
   ) {
     super(appRef, injector, document);
   }
 
-  open(
-    componentType: Type<any>,
-    config: DynamicDialogConfig
-  ): DynamicDialogRef {
+  open<T>(componentType: Type<T>, config: DynamicDialogConfig) {
     this.tracking.trackEvent(AdobeEvent.trackView, {
       category: config.header,
       action: 'modal-view',
       detail: pathOr(config.header, ['data', 'detail'], config),
       location: 'Modal',
     });
-    return super.open(componentType, config);
+    const modal = super.open(componentType, config);
+    this.stackRefs.push(modal);
+    return modal;
+  }
+
+  closeAll() {
+    this.stackRefs.forEach((ref) => ref.destroy());
   }
 }
