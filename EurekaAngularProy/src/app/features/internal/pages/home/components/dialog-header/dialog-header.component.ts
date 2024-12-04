5b1e8fad8670cac3b945d7303c2732ef99f79843
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-12-04 17:49:36 -0500

    DDCOBSIM-683 feat: add debt header for modals

---
+++ EurekaAngularProy/src/app/features/internal/pages/home/components/dialog-header/dialog-header.component.ts
@@ -0,0 +1,25 @@
+import { NgClass } from '@angular/common';
+import { Component, HostBinding } from '@angular/core';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { TimesIcon } from 'primeng/icons/times';
+
+import { ExcelService } from '../../../../../../shared/services/excel.service';
+
+@Component({
+  selector: 'cs-dialog-header',
+  standalone: true,
+  imports: [NgClass, TimesIcon],
+  templateUrl: './dialog-header.component.html',
+})
+export class DialogHeaderComponent {
+  @HostBinding('class') class = 'tw-w-full';
+
+  constructor(
+    public excelService: ExcelService,
+    public ref: DynamicDialogRef,
+  ) {}
+
+  hide() {
+    this.ref.close();
+  }
+}
