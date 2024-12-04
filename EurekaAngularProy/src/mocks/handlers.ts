38c5fc2f6ff5b88f9b0543041a168f4db256b8b4
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-12-04 18:01:28 -0500

    DDCOBSIM-683 fix: mock with rejected error

--- EurekaAngularProy/src/mocks/handlers.ts
+++ EurekaAngularProy/src/mocks/handlers.ts
@@ -4,6 +4,7 @@ import {
   type ProcessStatus,
   processStatus,
 } from '../app/shared/constants/process';
+import { type IErrorObj } from '../app/shared/models/error.model';
 import { environment } from '../environments/environment';
 
 const serverApi = (path: string) => {
@@ -53,19 +54,31 @@ export const handlers = [
         validating,
         validated,
         saving,
-        completed,
+        rejected,
       ];
       const selectedKey = Math.floor(counter / 5);
       return processSteps[selectedKey];
     };
-    while (status !== completed) {
+    while (status !== rejected) {
       counter++;
       status = getProcess(counter);
 
+      const errors: IErrorObj[] =
+        status === rejected
+          ? [
+              {
+                code: 1,
+                row: 10,
+                description:
+                  'El código deudor ingresado ya existe, el nombre existente se va a mantener',
+              },
+            ]
+          : [];
+
       yield HttpResponse.json({
         status,
-        errors: [],
-        rowsUploaded: -4,
+        errors: errors,
+        rowsUploaded: 2,
         rowsRejected: 2,
         advance: 100,
         phase: 3,
