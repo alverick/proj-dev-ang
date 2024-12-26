b7a68d87589de7b0ecfea9ba7131ee6891ef6597
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2024-12-26 10:18:31 -0500

    DDCOBSIM-741 fix: validate error when check last empty errors

--- EurekaAngularProy/src/app/features/internal/pages/home/components/dialog/dialog.component.ts
+++ EurekaAngularProy/src/app/features/internal/pages/home/components/dialog/dialog.component.ts
@@ -183,7 +183,7 @@ export class DialogComponent implements OnInit {
       .getRows(this.rowStart(), limit - this.rowStart() + 1)
       .forEach((row) => {
         const errorObjs = this.validateRow(row);
-        if (this.checkLastEmptyRows(errorObjs)) {
+        if (errorObjs.length > 0 && this.checkLastEmptyRows(errorObjs)) {
           this.lastRows.push(row.number);
         }
         errors.push(...errorObjs);
@@ -303,7 +303,6 @@ export class DialogComponent implements OnInit {
 
   public selectFiled({ currentFiles }: { currentFiles: File[] }) {
     this.uploaderFiles = currentFiles;
-    // this.excelService.errores = [];
   }
 
   async openSnackBar() {
