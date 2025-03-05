40b5f5e4adf1484b99dba404e12ae8281962381f
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-03-05 18:05:10 -0500

    BEBCS2-57 fix: fixed window before unload event

--- EurekaAngularProy/src/app/features/auth/pages/service-add/service-add.page.ts
+++ EurekaAngularProy/src/app/features/auth/pages/service-add/service-add.page.ts
@@ -53,10 +53,7 @@ export class ServiceAddPage implements OnDestroy {
   }
 
   @HostListener('window:beforeunload', ['$event'])
-  unloadHandler(event: Event) {
-    confirm(
-      'El registro de tu empresa no ha concluido, si sales ahora los cambios se perderán.',
-    );
+  unloadHandler(event: BeforeUnloadEvent) {
     event.preventDefault();
   }
 }
