b8672420955082c7fe31f02e1d088f0aab7b6f70
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-05-23 12:00:49 -0500

    fix(BEBCS2-189): activate affiliation

--- app/src/app/store/reducers/app-config.reducer.ts
+++ app/src/app/store/reducers/app-config.reducer.ts
@@ -13,7 +13,7 @@ export interface State {
 
 export const initialState: State = {
   loaded: false,
-  disabledAffiliation: true,
+  disabledAffiliation: false,
   showedCommission: false,
   showLoader: true,
 };
