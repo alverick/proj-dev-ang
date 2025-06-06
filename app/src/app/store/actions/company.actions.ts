29a4d50d69d539895e1fd71fe6396db78ea54f40
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-06-06 15:07:47 -0500

    fix(BEBCS2-218): remove details from store

--- app/src/app/store/actions/company.actions.ts
+++ app/src/app/store/actions/company.actions.ts
@@ -1,13 +1,13 @@
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
 
-import { type IDataEnterpriseModel } from '../../shared/models/data-enterprise.model';
+import { type AmountLimit } from '../../shared/models/data-enterprise.model';
 
 export const CompanyActions = createActionGroup({
   source: 'Company',
   events: {
     'Load Company': emptyProps(),
-    'Load Company Success': props<{ data: IDataEnterpriseModel }>(),
     'Load Company Failure': props<{ error: any }>(),
-    'Set Currency Limits': emptyProps(),
+    'Set Currency Limits': props<{ amountLimits: AmountLimit[] }>(),
+    'Set Company Loader': props<{ show: boolean }>(),
   },
 });
