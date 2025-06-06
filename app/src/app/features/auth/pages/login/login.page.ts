321689d7bef5c92075576870c7098ca4c3c57416
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-06-06 14:57:20 -0500

    feat(BEBCS2-218): integrate encryption service, added name for document number

--- app/src/app/features/auth/pages/login/login.page.ts
+++ app/src/app/features/auth/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, type OnInit, ViewChild } from '@angular/core';
+import { Component, inject, type OnInit, ViewChild } from '@angular/core';
 import {
   FormBuilder,
   FormsModule,
@@ -23,10 +23,12 @@ import { first } from 'rxjs/operators';
 import { environment } from '../../../../../environments/environment';
 import { internalFullRoutingNames } from '../../../../app-routing.collection';
 import { LabelControlComponent } from '../../../../shared/components/label-control/label-control.component';
+import { companyDocumentStorageName } from '../../../../shared/constants/company';
 import { errorsLoginForm } from '../../../../shared/constants/company-errors';
 import { AFFILIATION_SUSPENDED } from '../../../../shared/constants/message-service';
 import { loginResultStatus } from '../../../../shared/constants/session';
 import { ModelFormGroup } from '../../../../shared/models/forms';
+import { EncryptionService } from '../../../../shared/services/encryption.service';
 import { LoginService } from '../../../../shared/services/login.service';
 import { StorageService } from '../../../../shared/services/storage.service';
 import {
@@ -39,6 +41,7 @@ import { rucValidators } from '../../../../shared/validators/company-validators'
 import { appConfigFeature } from '../../../../store/reducers/app-config.reducer';
 import { authFullRoutingNames } from '../../auth-routing.names';
 import { LayoutFormComponent } from '../../components/layout-form/layout-form.component';
+import { isNotNilOrEmpty } from 'ramda-adjunct';
 
 const userData = environment.credentials[0];
 
@@ -71,7 +74,6 @@ interface LoginForm {
 })
 export class LoginPage implements OnInit {
   public loginForm: ModelFormGroup<LoginForm>;
-  public rememberMe = false;
 
   intentos: number;
   intentosRestantes = 6;
@@ -86,6 +88,8 @@ export class LoginPage implements OnInit {
   );
   attemptsLimit = 6;
 
+  encryptionService = inject(EncryptionService);
+
   constructor(
     private readonly formBuilder: FormBuilder,
     private readonly loginService: LoginService,
@@ -98,23 +102,35 @@ export class LoginPage implements OnInit {
   ) {}
 
   ngOnInit() {
-    const rucStr = this.cookieService.check('ruc')
-      ? this.cookieService.get('ruc')
+    this.loginForm = this.formBuilder.group({
+      ruc: ['', rucValidators],
+      psw: ['', [Validators.required, Validators.maxLength(25)]],
+      rememberMe: [false, Validators.required],
+    });
+    const rucStr = this.cookieService.check(companyDocumentStorageName)
+      ? this.cookieService.get(companyDocumentStorageName)
       : userData[0];
-
-    this.validationLogin(rucStr);
+    if (isNotNilOrEmpty(rucStr)) {
+      this.checkStatusLoginForm(rucStr);
+    }
   }
 
-  validationLogin(rucStr: string) {
-    if (rucStr) {
-      this.rememberMe = true;
-    }
-
-    this.loginForm = this.formBuilder.group({
-      ruc: [rucStr, rucValidators],
-      psw: [userData[1], [Validators.required, Validators.maxLength(25)]],
-      rememberMe: [this.rememberMe, Validators.required],
-    });
+  checkStatusLoginForm(rucStr: string) {
+    void this.encryptionService
+      .getSessionEncryptionKey('local')
+      .then((key) => {
+        return this.encryptionService.decryptData(rucStr, key);
+      })
+      .then((ruc) => {
+        this.loginForm.patchValue({ ruc, rememberMe: true });
+        if (isNotNilOrEmpty(userData[0]) && isNotNilOrEmpty(userData[1])) {
+          this.loginForm.patchValue({
+            ruc: userData[0],
+            psw: userData[1],
+            rememberMe: false,
+          });
+        }
+      });
   }
 
   get f() {
@@ -140,122 +156,158 @@ export class LoginPage implements OnInit {
   }
 
   public submitLogin() {
-    if (!this.loginForm.valid) {
-      return;
-    }
+    if (!this.loginForm.valid) return;
 
-    this.cookieService.delete('ruc');
+    this.cookieService.delete(companyDocumentStorageName);
+    const ruc = this.f.ruc.value;
+    const password = this.f.psw.value;
 
-    const actionParams = {
+    const actionParams = this.buildTrackingParams(
+      ruc,
+      this.passwordControl.unmasked,
+    );
+    this.tracking.setRuc(ruc);
+
+    this.loginService
+      .login(ruc, password)
+      .pipe(first())
+      .subscribe({
+        next: (value) => this.handleLoginResponse(value, actionParams),
+        error: (error: HttpErrorResponse) =>
+          this.handleLoginError(error, actionParams),
+      });
+  }
+
+  private buildTrackingParams(ruc: string, unmasked: boolean) {
+    return {
       category: 'Login',
       action: 'Click',
       label: 'Ingresar',
       state: 'Envío exitoso',
       metadata: [
-        {
-          key: 'TipoDocumento',
-          value: 'RUC',
-        },
-        {
-          key: 'NumeroDocumento',
-          value: this.f.ruc.value,
-        },
-        {
-          key: 'MostrarPassword',
-          value: this.passwordControl.unmasked,
-        },
+        { key: 'TipoDocumento', value: 'RUC' },
+        { key: 'NumeroDocumento', value: ruc },
+        { key: 'MostrarPassword', value: unmasked },
       ],
     };
+  }
 
-    this.tracking.setRuc(this.f.ruc.value);
+  private handleLoginResponse(value: any, actionParams: any) {
+    this.intentos = value.paramNum;
+    this.intentosRestantes = this.attemptsLimit - this.intentos;
+    this.codRespuesta = value.codRespuesta;
 
-    if (this.loginForm.valid) {
-      this.loginService
-        .login(this.f.ruc.value, this.f.psw.value)
-        .pipe(first())
-        .subscribe({
-          next: (value) => {
-            this.storageService.setIntentos(value.paramNum);
-            this.intentos = value.paramNum;
+    if (value.paramStr === 'Un session ya se encuentra activa') {
+      this.showSessionActiveModal(value, actionParams);
+      return;
+    }
 
-            this.intentosRestantes = this.attemptsLimit - this.intentos;
-            this.codRespuesta = value.codRespuesta;
-            if (value.paramStr === 'Un session ya se encuentra activa') {
-              this.showModal('Existe una Sesión Activa', '');
-              this.sendAdobeTrack({
-                ...actionParams,
-                state: 'Intención de envío',
-                typeError: value.paramStr,
-              });
-            } else if (
-              value.estado === true &&
-              this.intentos <= this.attemptsLimit
-            ) {
-              if (this.f.rememberMe.value === true) {
-                const expire = new Date();
-                expire.setDate(expire.getDate() + 25);
-                this.cookieService.set('ruc', this.f.ruc.value, expire);
-              }
+    if (value.estado && this.intentos <= this.attemptsLimit) {
+      this.processSuccessfulLogin(value, actionParams);
+      return;
+    }
 
-              window.sessionStorage.setItem('username', this.f.ruc.value);
-              void this.router.navigate([internalFullRoutingNames.HOME]);
-              this.sendAdobeTrack(actionParams);
-              this.tracking.trackEvent(AdobeEvent.successLogin);
-            } else if (
-              this.intentos >= this.attemptsLimit ||
-              value.paramStr === 'Vuelva a intentarlo mas tarde' ||
-              value.paramStr === 'El usuario esta bloqueado'
-            ) {
-              this.showModal(
-                'Contraseña Incorrecta',
-                'Tu cuenta ha sido bloqueada por seguridad, inténtalo nuevamente en 60 minutos. Si tienes problemas para ingresar a tu cuenta, contáctanos por whatsapp al 993 119 001.',
-              );
-              this.intento6 = true;
-              this.sendAdobeTrack({
-                ...actionParams,
-                state: 'Intención de envío',
-                typeError: value.paramStr,
-              });
-            } else if (this.intentos < this.attemptsLimit) {
-              if (this.codRespuesta === loginResultStatus.errorCredentials) {
-                this.showModal(
-                  'Contraseña incorrecta',
-                  `Lo sentimos tu contraseña es incorrecta, verifícala o vuelve a intentarlo. Tienes  ${this.intentosRestantes} intentos restantes.`,
-                );
-                this.sendAdobeTrack({
-                  ...actionParams,
-                  state: 'Intención de envío',
-                  typeError: 'Contraseña incorrecta',
-                });
-              } else if (this.codRespuesta === loginResultStatus.userInactive) {
-                this.showModal(
-                  'Tu cuenta está siendo procesada',
-                  'Estamos procesando la información de tu registro,' +
-                    ' esto puede tomar un máximo 24 horas hábiles. Cuando esté lista te enviaremos un mail de Bienvenida.',
-                  'Entendido',
-                );
-                this.sendAdobeTrack({
-                  ...actionParams,
-                  state: 'Intención de envío',
-                  typeError: 'Tu cuenta está siendo procesada',
-                });
-              }
-            }
-          },
-          error: (error: HttpErrorResponse) => {
-            if (error.status === 500) {
-              this.showModal(
-                'Error',
-                'Error del servidor comuníquese con el administrador',
-              );
-              this.sendAdobeTrack({
-                ...actionParams,
-                state: 'Intención de envío',
-                typeError: 'Error del servidor',
-              });
-            }
-          },
-        });
+    if (
+      this.intentos >= this.attemptsLimit ||
+      ['Vuelva a intentarlo mas tarde', 'El usuario esta bloqueado'].includes(
+        value.paramStr,
+      )
+    ) {
+      this.handleBlockedUser(value, actionParams);
+      return;
+    }
+
+    this.handleFailedAttempt(actionParams);
+  }
+
+  private showSessionActiveModal(value: any, actionParams: any) {
+    this.showModal('Existe una sesión activa', '');
+    this.sendAdobeTrack({
+      ...actionParams,
+      state: 'Intención de envío',
+      typeError: value.paramStr,
+    });
+  }
+
+  private async processSuccessfulLogin(value: any, actionParams: any) {
+    if (this.f.rememberMe.value) {
+      const expire = new Date();
+      expire.setDate(expire.getDate() + 25);
+      this.cookieService.set(
+        companyDocumentStorageName,
+        this.f.ruc.value,
+        expire,
+      );
+      const processedDocument = await this.encryptionService.encryptData(
+        this.f.ruc.value,
+        await this.encryptionService.getSessionEncryptionKey('local'),
+      );
+      this.cookieService.set(
+        companyDocumentStorageName,
+        processedDocument,
+        expire,
+      );
+    }
+
+    window.sessionStorage.setItem('username', value.id);
+    void this.router.navigate([internalFullRoutingNames.HOME]);
+
+    this.sendAdobeTrack(actionParams);
+    this.tracking.trackEvent(AdobeEvent.successLogin);
+  }
+
+  private handleBlockedUser(value: any, actionParams: any) {
+    this.showModal(
+      'Contraseña Incorrecta',
+      'Tu cuenta ha sido bloqueada por seguridad, inténtalo nuevamente en 60 minutos. ' +
+        'Si tienes problemas para ingresar a tu cuenta, contáctanos por whatsapp al 993 119 001.',
+    );
+    this.intento6 = true;
+
+    this.sendAdobeTrack({
+      ...actionParams,
+      state: 'Intención de envío',
+      typeError: value.paramStr,
+    });
+  }
+
+  private handleFailedAttempt(actionParams: any) {
+    if (this.codRespuesta === loginResultStatus.errorCredentials) {
+      this.showModal(
+        'Contraseña incorrecta',
+        `Lo sentimos tu contraseña es incorrecta, verifícala o vuelve a intentarlo. Tienes ${this.intentosRestantes} intentos restantes.`,
+      );
+      this.sendAdobeTrack({
+        ...actionParams,
+        state: 'Intención de envío',
+        typeError: 'Contraseña incorrecta',
+      });
+    } else if (this.codRespuesta === loginResultStatus.userInactive) {
+      this.showModal(
+        'Tu cuenta está siendo procesada',
+        'Estamos procesando la información de tu registro, esto puede tomar un máximo 24 horas hábiles. ' +
+          'Cuando esté lista te enviaremos un mail de Bienvenida.',
+        'Entendido',
+      );
+      this.sendAdobeTrack({
+        ...actionParams,
+        state: 'Intención de envío',
+        typeError: 'Tu cuenta está siendo procesada',
+      });
+    }
+  }
+
+  private handleLoginError(error: HttpErrorResponse, actionParams: any) {
+    if (error.status === 500) {
+      this.showModal(
+        'Error',
+        'Error del servidor comuníquese con el administrador',
+      );
+      this.sendAdobeTrack({
+        ...actionParams,
+        state: 'Intención de envío',
+        typeError: 'Error del servidor',
+      });
     }
   }
 
