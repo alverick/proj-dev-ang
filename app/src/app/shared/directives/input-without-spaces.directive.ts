a8764abd2656cee202fa60bd28740aa3ea4caf29
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-06-25 17:08:36 -0500

    fix(BEBCS2-189): update max length when user paste in the control

--- app/src/app/shared/directives/input-without-spaces.directive.ts
+++ app/src/app/shared/directives/input-without-spaces.directive.ts
@@ -14,17 +14,25 @@ export class InputWithoutSpacesDirective {
   @HostListener('paste', ['$event'])
   onPaste(event: ClipboardEvent) {
     event.preventDefault();
-    const clipboardData = event.clipboardData?.getData('text') || '';
-    const sanitized = this.sanitize(clipboardData);
     const input = event.target as HTMLInputElement;
+    const clipboardData = event.clipboardData?.getData('text') || '';
+    const sanitizedPaste = this.sanitize(clipboardData);
 
     const start = input.selectionStart ?? 0;
     const end = input.selectionEnd ?? 0;
-    const newValue =
-      input.value.slice(0, start) + sanitized + input.value.slice(end);
+
+    const before = input.value.slice(0, start);
+    const after = input.value.slice(end);
+    const maxLength = input.maxLength > 0 ? input.maxLength : Infinity;
+
+    const availableSpace = maxLength - before.length - after.length;
+    const truncatedPaste = sanitizedPaste.slice(0, availableSpace);
+
+    const newValue = before + truncatedPaste + after;
     input.value = newValue;
 
-    input.setSelectionRange(start + sanitized.length, start + sanitized.length);
+    const cursorPosition = start + truncatedPaste.length;
+    input.setSelectionRange(cursorPosition, cursorPosition);
   }
 
   @HostListener('blur', ['$event'])
