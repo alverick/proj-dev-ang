266988b218e9c5b3653df3dfbba4d4865cd5cf85
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-06-30 15:28:55 -0500

    fix(BEBCS2-189): fix regex for service

--- app/src/app/shared/components/service-step-info/service-step-info.component.ts
+++ app/src/app/shared/components/service-step-info/service-step-info.component.ts
@@ -22,7 +22,7 @@ import { InputWithoutSpacesDirective } from '../../directives/input-without-spac
 import { IErrorMessages, type ModelFormGroup } from '../../models/forms';
 import { type CompanyAccounts } from '../../services/company.service';
 import { type ServiceFormValue } from '../../services/services-forms.service';
-import { namePattern } from '../../validators/company-validators';
+import { namePattern } from '../../validators/service-validators';
 import { LabelControlComponent } from '../label-control/label-control.component';
 import { MessageAlertComponent } from '../message-alert/message-alert.component';
 import { ServiceChannelChipComponent } from '../service-channel-chip/service-channel-chip.component';
