d5f60b7a99a9ecbd366e0872b8439aee82295ad6
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-08-12 10:27:08 -0500

    chore(BEBCS2-358): removed items from index transform

--- app/src/index-html.transform.ts
+++ app/src/index-html.transform.ts
@@ -11,33 +11,17 @@ const defaultRootUrl = 'https://cobrosimple.dev.interbank.pe';
  * - `production` - Production environment with optimizations.
  * - `uat` - User Acceptance Testing (UAT) environment for validation.
  */
-type EnvironmentType = 'default' | 'dev' | 'production' | 'uat';
+type EnvironmentType =
+  | 'default'
+  | 'dev'
+  | 'production'
+  | 'legacy-production'
+  | 'uat';
 
 /**
  * Configuration settings for each environment.
  */
 type EnvironmentConfig = {
-  /**
-   * Script file paths for third-party services.
-   */
-  scripts: {
-    /**
-     * The local path for the Hotjar tracking script.
-     * Used for user behavior analytics.
-     *
-     * @example "/assets/scripts/hotjar.js"
-     */
-    hotjar: string;
-
-    /**
-     * The local path for the New Relic monitoring script.
-     * Used for performance tracking and error logging.
-     *
-     * @example "/assets/scripts/newrelic.js"
-     */
-    newRelic: string;
-  };
-
   /**
    * The base domain URL for the environment.
    *
@@ -48,31 +32,18 @@ type EnvironmentConfig = {
 
 const settings: Record<EnvironmentType, EnvironmentConfig> = {
   default: {
-    scripts: {
-      newRelic: '',
-      hotjar: '',
-    },
     url: defaultRootUrl,
   },
   dev: {
-    scripts: {
-      newRelic: 'assets/scripts/new-relic-dev.js',
-      hotjar: '',
-    },
     url: defaultRootUrl,
   },
   uat: {
-    scripts: {
-      newRelic: 'assets/scripts/new-relic-dev.js',
-      hotjar: 'assets/scripts/hotjar.js',
-    },
     url: 'https://cobrosimple.uat.interbank.pe',
   },
   production: {
-    scripts: {
-      newRelic: 'assets/scripts/new-relic.js',
-      hotjar: 'assets/scripts/hotjar.js',
-    },
+    url: 'https://cobrosimple.interbank.pe',
+  },
+  'legacy-production': {
     url: 'https://cobrosimple.interbank.pe',
   },
 };
@@ -80,22 +51,6 @@ const settings: Record<EnvironmentType, EnvironmentConfig> = {
 function replaceDomainUrl(configuration: EnvironmentType, indexHtml: string) {
   if (settings[configuration].url !== defaultRootUrl) {
     return indexHtml.replaceAll(defaultRootUrl, settings[configuration].url);
-  }
-  return indexHtml;
-}
-
-function includeScripts(configuration: EnvironmentType, indexHtml: string) {
-  let scripts = '';
-  forEachObjIndexed(function (value) {
-    if (!isEmpty(value)) {
-      scripts += `<script src="${value}"></script>`;
-    }
-  }, settings[configuration].scripts);
-  if (!isEmpty(scripts)) {
-    const enHeadPosition = indexHtml.indexOf('</head>');
-    return `${indexHtml.slice(0, enHeadPosition)}
-            ${scripts}
-            ${indexHtml.slice(enHeadPosition)}`;
   }
   return indexHtml;
 }
