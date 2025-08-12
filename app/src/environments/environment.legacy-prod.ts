2b9daf0fd3466b043ac10e64de7e90c185bf514a
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-08-12 10:26:07 -0500

    chore(BEBCS2-358): added legacy production environment

---
+++ app/src/environments/environment.legacy-prod.ts
@@ -0,0 +1,17 @@
+import { NgxLoggerLevel } from 'ngx-logger';
+
+import type { IEnvironment } from './constants';
+import { environmentDefault, serverUrl } from './constants';
+
+export const environment: IEnvironment = {
+  ...environmentDefault,
+  development: false,
+  production: true,
+  END_POINT: serverUrl.prod,
+  logLevel: NgxLoggerLevel.OFF,
+  serverLogLevel: NgxLoggerLevel.ERROR,
+  adobe:
+    'https://assets.adobedtm.com/df637a308f4c/ec7ac20e39e1/launch-f36536511e92.min.js',
+  hotjarSiteId: '2944859',
+  hotjarVersion: '6',
+};
