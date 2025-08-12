343a195c9846961b0984f19f465617c5b94a4f9c
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-08-12 10:26:38 -0500

    chore(BEBCS2-358): restored environment production devsecops version

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,17 +1,16 @@
 import { NgxLoggerLevel } from 'ngx-logger';
 
 import type { IEnvironment } from './constants';
-import { environmentDefault, serverUrl } from './constants';
+import { environmentDefault } from './constants';
 
 export const environment: IEnvironment = {
   ...environmentDefault,
   development: false,
   production: true,
-  END_POINT: serverUrl.prod,
+  END_POINT: '#{API_URL}#',
   logLevel: NgxLoggerLevel.OFF,
   serverLogLevel: NgxLoggerLevel.ERROR,
-  adobe:
-    'https://assets.adobedtm.com/df637a308f4c/ec7ac20e39e1/launch-f36536511e92.min.js',
-  hotjarSiteId: '2944859',
-  hotjarVersion: '6',
+  adobe: '#{ADOBE_URL}',
+  hotjarSiteId: '#{HOTJAR_SITE_ID}',
+  hotjarVersion: '#{HOTJAR_VERSION}',
 };
