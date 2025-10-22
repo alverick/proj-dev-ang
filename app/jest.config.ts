261f6a80bc55e41c1a7e450d7e463528744c25b5
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-10-22 17:27:04 -0500

    chore(BEBCS2-378): config jest-unit

--- app/jest.config.ts
+++ app/jest.config.ts
@@ -108,7 +108,17 @@ const config: Config = {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputName: 'unit-test-result.xml',
+        ancestorSeparator: ' › ',
+        usePathForSuiteName: 'true',
+      },
+    ],
+  ],
 
   // Automatically reset mock state before every test
   // resetMocks: false,
