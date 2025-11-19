379aed77dc0009de60b8a45276ed333144c12481
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-11-19 10:24:00 -0500

    fix(BEBCS2-447): replace timesicon component with i tag

--- app/src/app/features/internal/pages/home/components/dialog-header/dialog-header.component.ts
+++ app/src/app/features/internal/pages/home/components/dialog-header/dialog-header.component.ts
@@ -1,13 +1,13 @@
-import { NgClass } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common'; // Import CommonModule
 import { Component, HostBinding } from '@angular/core';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
-import { TimesIcon } from 'primeng/icons/times';
+import { TimesIcon } from 'primeng/icons';
 
 import { ExcelService } from '../../../../../../shared/services/excel.service';
 
 @Component({
   selector: 'cs-dialog-header',
-  imports: [NgClass, TimesIcon],
+  imports: [CommonModule, NgClass, TimesIcon],
   templateUrl: './dialog-header.component.html',
 })
 export class DialogHeaderComponent {
