d3d2ebd6cdf7a176b32af14d2f52c9671a423e44
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2025-12-02 12:04:57 -0500

    fix(BEBCS2-447): updated dependencies for pages

--- app/src/app/features/internal/internal.routes.ts
+++ app/src/app/features/internal/internal.routes.ts
@@ -1,8 +1,20 @@
 import { type Routes } from '@angular/router';
 
 import { appRoutingNames } from '../../app-routing.names';
+import { DebtDataService, IpInfoDataService } from '../../shared/data';
 import { AuthGuard } from '../../shared/guards/auth.guard';
 import { GtpOutputGuard } from '../../shared/guards/gtp-output.guard';
+import {
+  DigitalDataService,
+  ServiceService,
+  ServicesFormsService,
+  SettingsStorageService,
+} from '../../shared/services';
+import { AfiliacionService } from '../../shared/services/afiliacion.service';
+import { DynamicDialogService } from '../../shared/services/dynamic-dialog.service';
+import { HomeService } from '../../shared/services/home.service';
+import { LoadBarService } from '../../shared/services/load-bar.service';
+import { LoadFileService } from '../../shared/services/load-file.service';
 import { InternalComponent } from './internal.component';
 import {
   internalFullRoutingChildNames,
@@ -24,16 +36,35 @@ import {
   CompanyDataResolver,
   CompanyServicesResolver,
 } from './resolvers';
+import {
+  CompanyConfigurationService,
+  CompanyServicesService,
+  MovementsService,
+  SelectAllTableService,
+} from './services';
 
 export const INTERNAL_ROUTES: Routes = [
   {
     path: appRoutingNames.EMPTY,
     component: InternalComponent,
+    providers: [
+      AfiliacionService,
+      DynamicDialogService,
+      SettingsStorageService,
+    ],
     children: [
       {
         path: internalRoutingNames.HOME,
         component: HomePage,
         canActivate: [AuthGuard, GtpOutputGuard],
+        providers: [
+          HomeService,
+          LoadFileService,
+          LoadBarService,
+          MovementsService,
+          DebtDataService,
+          SelectAllTableService,
+        ],
       },
       {
         path: internalRoutingNames.COMPANY,
@@ -42,6 +73,7 @@ export const INTERNAL_ROUTES: Routes = [
           company: CompanyDataResolver,
         },
         canActivate: [AuthGuard, GtpOutputGuard],
+        providers: [CompanyDataResolver, CompanyConfigurationService],
       },
       {
         path: internalRoutingNames.SERVICES,
@@ -49,6 +81,15 @@ export const INTERNAL_ROUTES: Routes = [
         resolve: {
           company: CompanyDataResolver,
         },
+        providers: [
+          CompanyDataResolver,
+          CompanyServicesResolver,
+          CompanyServicesService,
+          ServicesFormsService,
+          ServiceService,
+          DigitalDataService,
+          IpInfoDataService,
+        ],
         children: [
           {
             path: appRoutingNames.EMPTY,
