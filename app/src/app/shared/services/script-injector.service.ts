48fe2a1af35b5667f821f2d2eebf0a96dab962eb
Jorge Suarez <jsuarezq@proveedor.intercorp.com.pe>
2026-02-20 16:53:54 -0500

    fix(BEBCS2-619): upgrade script injector for detect loaded scripts

--- app/src/app/shared/services/script-injector.service.ts
+++ app/src/app/shared/services/script-injector.service.ts
@@ -1,32 +1,66 @@
 import { DOCUMENT, inject, Injectable } from '@angular/core';
-import { type Observable, ReplaySubject } from 'rxjs';
-@Injectable({
-  providedIn: 'root',
-})
+import { Observable, ReplaySubject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
 export class ScriptInjectorService {
   private readonly document = inject<Document>(DOCUMENT);
 
-  private loadedLibraries: Record<string, ReplaySubject<boolean>> = {};
+  private readonly scripts = new Map<string, ReplaySubject<boolean>>();
 
-  loadScript(id: string, src: string): Observable<boolean> {
-    if (this.loadedLibraries[src]) {
-      return this.loadedLibraries[src].asObservable();
+  loadScript(
+    id: string,
+    src: string,
+    options?: Partial<HTMLScriptElement>,
+  ): Observable<boolean> {
+    if (this.scripts.has(id)) {
+      return this.scripts.get(id).asObservable();
     }
 
-    this.loadedLibraries[src] = new ReplaySubject();
+    const subject = new ReplaySubject<boolean>(1);
+    this.scripts.set(id, subject);
+
+    const existingScript = this.document.getElementById(
+      id,
+    ) as HTMLScriptElement | null;
+
+    if (existingScript) {
+      if (existingScript.dataset['loaded'] === 'true') {
+        subject.next(true);
+        subject.complete();
+      } else {
+        existingScript.addEventListener('load', () => {
+          subject.next(true);
+          subject.complete();
+        });
+
+        existingScript.addEventListener('error', () => {
+          subject.error(false);
+        });
+      }
+
+      return subject.asObservable();
+    }
 
     const script = this.document.createElement('script');
     script.id = id;
     script.type = 'text/javascript';
-    script.async = true;
     script.src = src;
+    script.async = true;
+
+    Object.assign(script, options);
+
     script.onload = () => {
-      this.loadedLibraries[src].next(true);
-      this.loadedLibraries[src].complete();
+      script.dataset['loaded'] = 'true';
+      subject.next(true);
+      subject.complete();
+    };
+
+    script.onerror = () => {
+      subject.error(false);
     };
 
     this.document.head.appendChild(script);
 
-    return this.loadedLibraries[src].asObservable();
+    return subject.asObservable();
   }
 }
